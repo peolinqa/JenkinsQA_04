53e48899057e66e087cdd87b0fe5a42a04e33096
SergeiDemyanenko <Sergei.Demyanenko@gmail.com>
2022-05-10 23:02:30 -0500

    Ignore my test (#3)
    Ignore my test

--- src/test/java/SergeyTest.java
+++ src/test/java/SergeyTest.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class SergeyTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testFirstSelenium() throws InterruptedException {
         getDriver().get("https://google.com");
