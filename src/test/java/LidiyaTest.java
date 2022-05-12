cd2d965b8008b7c913dd1b2350b1673a5ca74bb0
Lidiya Makarova <li-ma@mail.ru>
2022-05-13 01:46:51 +0300

    LiMaTest commit

--- src/test/java/LidiyaTest.java
+++ src/test/java/LidiyaTest.java
@@ -6,9 +6,6 @@ import runner.BaseTest;
 
 public class LidiyaTest extends BaseTest {
 
-    WebElement fieldLogin = getDriver().findElement(By.id("user-identifier-input"));
-    WebElement fieldPassword = getDriver().findElement(By.id("password-input"));
-    WebElement submitButton = getDriver().findElement(By.id("user-identifier-input"));
 
     @Test
     public void lidiyaMakarovaTestForBBCPositiveCheck() throws InterruptedException {
@@ -17,12 +14,15 @@ public class LidiyaTest extends BaseTest {
         WebElement iconSignIn = getDriver().findElement(By.xpath("//*[@id='idcta-username']"));
         iconSignIn.click();
 
+        WebElement fieldLogin = getDriver().findElement(By.id("user-identifier-input"));
+        WebElement fieldPassword = getDriver().findElement(By.id("password-input"));
+        WebElement submitButton = getDriver().findElement(By.id("submit-button"));
 
         fieldLogin.sendKeys("li-ma@mail.ru");
         fieldPassword.sendKeys("R4e3w2Q1");
         submitButton.click();
 
-        Assert.assertEquals("https://www.bbc.com/", getDriver().getCurrentUrl());
+        Assert.assertEquals(getDriver().getCurrentUrl(),"https://www.bbc.com/");
 
         WebElement iconYourAccount = getDriver().findElement(By.id("idcta-username"));
         Assert.assertEquals("Your account", iconYourAccount.getText());
@@ -31,13 +31,19 @@ public class LidiyaTest extends BaseTest {
     @Test
     public void lidiyaMakarovaTestForBBCNegativeCheck() throws InterruptedException {
         getDriver().get("https://www.bbc.com/");
+        WebElement iconSignIn = getDriver().findElement(By.xpath("//*[@id='idcta-username']"));
+        iconSignIn.click();
+
+        WebElement fieldLogin = getDriver().findElement(By.id("user-identifier-input"));
+        WebElement fieldPassword = getDriver().findElement(By.id("password-input"));
+        WebElement submitButton = getDriver().findElement(By.id("submit-button"));
 
         fieldLogin.sendKeys("li-ma@mail.ru");
         fieldPassword.sendKeys("R4e3w2Q2");
         submitButton.click();
 
         WebElement errorText = getDriver().findElement(By.xpath("//*[@id='form-message-password']"));
-        Assert.assertEquals("Uh oh, that password doesn’t match that account. Please try again.", errorText.getText());
+        Assert.assertEquals(errorText.getText(),"Uh oh, that password doesn’t match that account. Please try again.");
     }
 }
 
