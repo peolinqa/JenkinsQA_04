9c87de4b56d18af7b52d79fc0a4253c7e5f96601
BOksanaV <BOV1702@gmail.com>
2022-05-12 11:33:18 -0400

    Deleted spaces

--- src/test/java/GroupBugHuntersTest.java
+++ src/test/java/GroupBugHuntersTest.java
@@ -4,24 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-
 public class GroupBugHuntersTest extends BaseTest {
 
     @Test
     public void testOksanaBakatova()  {
 
-
         getDriver().get("https://www.etsy.com");
-
         WebElement searchBox = getDriver().findElement(By.xpath("//input[@name='search_query']"));
         WebElement searchButton = getDriver().findElement(By.xpath("//button[@type ='submit']"));
-
         searchBox.sendKeys("soap flower bouquet");
         searchButton.click();
-
         searchBox = getDriver().findElement(By.xpath("//input[@name='search_query']"));
         Assert.assertEquals(searchBox.getAttribute("value"), "soap flower bouquet");
 
     }
-
 }
