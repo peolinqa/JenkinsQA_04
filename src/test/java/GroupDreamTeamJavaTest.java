1576f5e3334d29ca5793f2104f234a4960d859fc
sashokdzr <43587497+sashokdzr@users.noreply.github.com>
2022-05-16 00:04:43 +0300

    Add_First_PR_by_AD_Dream_Team (#85)
    * Add_First_PR_by_AD_Dream_Team

--- src/test/java/GroupDreamTeamJavaTest.java
+++ src/test/java/GroupDreamTeamJavaTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -32,4 +33,44 @@ public class GroupDreamTeamJavaTest extends BaseTest {
 
         Assert.assertEquals(x.getText(), "Вот почему мы всегда пишем селекторы на XPath");
     }
+    @Test
+    public void findAuto_AliaksandrD() throws InterruptedException {
+        getDriver().get("https://av.by");
+
+        WebElement markaAuto = getDriver()
+                .findElement(By.xpath("//button[@name='p-6-0-2-brand']"));
+        markaAuto.click();
+
+        WebElement searchMarkaAuto = getDriver()
+                .findElement(By.xpath("//input[@placeholder='Поиск']"));
+        searchMarkaAuto.sendKeys("Audi" + Keys.ENTER);
+
+        Thread.sleep(4000);
+
+        WebElement model = getDriver()
+                .findElement(By.xpath("//button[@name='p-6-0-3-model']"));
+        model.click();
+
+        Thread.sleep(4000);
+
+        WebElement searchModel = getDriver()
+                .findElement(By.xpath("//button[@data-item-label='80']"));
+        searchModel.click();
+
+        Thread.sleep(4000);
+
+        WebElement buttonShow = getDriver()
+                .findElement(By.xpath("//a[@class='button button--secondary button--block']"));
+        buttonShow.click();
+
+        Thread.sleep(6000);
+        //Title result:
+        //Купить Audi 80 | 310 объявлений о продаже на av.by | Цены, характеристики, фото.
+        String[] title = getDriver()
+                .getTitle()
+                .split(" ");
+        String actual = title[1] + " " + title[2];
+        Assert.assertEquals(actual, "Audi 80");
+    }
+
 }
