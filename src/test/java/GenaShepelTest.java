95bda1b9b5a41ed3c03ea031041cad1929a619fe
Gena Shepel <genashepel@gmail.com>
2022-05-14 23:38:07 -0400

    05/14/2022_11:37

--- src/test/java/GenaShepelTest.java
+++ src/test/java/GenaShepelTest.java
@@ -1,5 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -48,6 +50,58 @@ public class GenaShepelTest extends BaseTest {
         //searchButton = getDriver().findElement(By.className("yosegi-InlineWhatWhere-primaryButton"));
 
 
+    }
+
+    @Test
+    public void testMainMenuOfStartPage() throws  InterruptedException{
+
+        String startMenu = "http://www.99-bottles-of-beer.net/";
+        String browseLanguagesMenu = "http://www.99-bottles-of-beer.net/abc.html";
+        String serchLanguagesMenu = "http://www.99-bottles-of-beer.net/search.html";
+        String topListMenu = "http://www.99-bottles-of-beer.net/toplist.html";
+        String guestBookMenu = "http://www.99-bottles-of-beer.net/guestbookv2.html";
+        String submitNewLanguages = "http://www.99-bottles-of-beer.net/submitnewlanguage.html";
+
+
+        String[] expectedResultArr = {
+                startMenu,
+                browseLanguagesMenu,
+                serchLanguagesMenu,
+                topListMenu,
+                guestBookMenu,
+                submitNewLanguages
+        };
+
+        String pathStartMenu = "//*[@id='menu']/li/a[@href='/']";
+        String pathBrowseLanguagesMenu = "//*[@id='menu']/li/a[@href='/abc.html']";
+        String pathSerchLanguagesMenu = "//*[@id='menu']/li/a[@href='/search.html']";
+        String pathTopListMenu = "//*[@id='menu']/li/a[@href='/toplist.html']";
+        String pathGuestBookMenu = "//*[@id='menu']/li/a[@href='/guestbookv2.html']";
+        String pathSubmitNewLanguages = "//*[@id='menu']/li/a[@href='/submitnewlanguage.html']";
+
+        String[] pathArr = {
+                pathStartMenu,
+                pathBrowseLanguagesMenu,
+                pathSerchLanguagesMenu,
+                pathTopListMenu,
+                pathGuestBookMenu,
+                pathSubmitNewLanguages
+        };
+
+
+        for(int i = 0; i < pathArr.length; i++){
+            getDriver().get(expectedResultArr[i]);
+            //Thread.sleep(3000);
+            for(int j = 0; j < pathArr.length; j++) {
+                WebElement nextPage = getDriver().findElement(By.xpath(pathArr[j]));
+                nextPage.click();
+                String actualResult = getDriver().getCurrentUrl();
+                String expectedResult = expectedResultArr[j];
+                Assert.assertEquals(expectedResult, actualResult);
+                //Thread.sleep(3000);
+            }
+        }
+        getDriver().quit();
     }
 
 }
