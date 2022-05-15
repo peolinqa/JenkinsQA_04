6597f1df089a456908b650c200c08aedc58dd2cc
Anton Sadchikov <aasadchikov@gmail.com>
2022-05-15 00:36:31 -0700

    Anton Sadchikov: HomeWork 11 - first test

--- src/test/java/AntonSadchikovTest.java
+++ src/test/java/AntonSadchikovTest.java
@@ -8,14 +8,12 @@ public class AntonSadchikovTest extends BaseTest {
 
     @Test
     public void testDiceSelenium() throws InterruptedException {
-
         getDriver().get("https://dice.com");
 
         WebElement searchBox = getDriver().findElement(By.id("typeaheadInput"));
         WebElement searchButton = getDriver().findElement(By.id("submitSearch-button"));
 
         searchBox.sendKeys("test");
-
         searchButton.click();
 
         Thread.sleep(2000);
