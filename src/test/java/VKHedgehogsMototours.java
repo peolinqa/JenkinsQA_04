666eb33d4c5f5509340779ad166d80ace8f04edb
Valentin <cricket666@yandex.ru>
2022-05-15 11:40:25 +0300

    Created FirstTests

---
+++ src/test/java/VKHedgehogsMototours.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class VKHedgehogsMototours extends BaseTest {
+
+    @Test
+    public void testSearchBox() throws InterruptedException {
+
+        getDriver().get("https://moto.tours/");
+
+        WebElement serachButton = getDriver().findElement(By.xpath("//div[@class='search-title-button-text']"));
+
+        serachButton.click();
+        WebElement searchBox = getDriver().findElement(By.xpath("//*[@class='search-title-form-input']"));
+
+        searchBox .sendKeys("Кофры", Keys.ENTER);
+
+        WebElement searchBoxEnd = getDriver().findElement(By.xpath("//*[@class='search-suggest']"));
+        Assert.assertEquals(searchBoxEnd.getAttribute("value"), "Кофры");
+
+    }
+}
