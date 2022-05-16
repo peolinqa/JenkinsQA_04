6d262f4623e8dcb5deac6ef2ec3004fc1b6cd3c5
Nina Burson <60827858+NinaBurson@users.noreply.github.com>
2022-05-16 10:51:43 -0400

    my first test (#101)

---
+++ src/test/java/NinaTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class NinaTest extends BaseTest {
+
+    @Test
+    public void testTest(){
+
+        getDriver().get("http://automationpractice.com/index.php");
+
+        WebElement searchField = getDriver().findElement(By.id("search_query_top"));
+        searchField.sendKeys("dress\n");
+
+        WebElement item = getDriver().findElement(By.xpath("//a[contains(text(),'Printed Summer Dress')]"));
+        item.click();
+
+        WebElement pageItem = getDriver().findElement(By.xpath("//h1"));
+
+        Assert.assertEquals(pageItem.getText(), "Printed Summer Dress");
+
+    }
+
+
+}
