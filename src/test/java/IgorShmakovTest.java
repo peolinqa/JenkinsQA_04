e5133b73682996edc848c451bea857a284f4a5cd
Chupa Chuck <garyisnottooold@gmail.com>
2022-05-15 22:37:38 -0700

    remove obsolete comments

--- src/test/java/IgorShmakovTest.java
+++ src/test/java/IgorShmakovTest.java
@@ -14,26 +14,15 @@ public class IgorShmakovTest extends BaseTest {
 
     @Test
     public void searchMercedesVinAndDecodeIt() {
-        // This method search inventory of the Pick-n-Pull vehicle yard for Mercedes CLK vehicles
-        // Get first vehicle found, open it page to get unique VIN (Vehicle Identification Number)
-        // Decode VIN number using another website with database of VIN numbers
-        // Verify that decoded data contains that VIN belongs to CLK model
-
-        //TO_DO
-        // need to automate submiting search form. For now use hardcoded url with search for Mercedes
         getDriver().get("https://www.picknpull.com/check-inventory/vehicle-search?make=182&model=3611&distance=25&zip=95123&year=");
 
-        // wait to have first test result, then open it
         WebElement firstSearchResult = new WebDriverWait(getDriver(), 10).until(
                 ExpectedConditions.elementToBeClickable(
                         By.xpath("//*/div[@class='fixed-table-body']/table//img[contains(@alt, 'Mercedes')][1]")));
         firstSearchResult.click();
 
-        // Expecting URL has VIN number at the end it is always 17 char long. Get the VIN and assign to variable
         String vinNumber = getDriver().getCurrentUrl().substring(getDriver().getCurrentUrl().length() - 17);
 
-        //TO_DO make selection of brand from main page
-        // Lets decode VIN and get some info about vehicle it belongs to, assuming that vin belogs to Mercedes brand
         getDriver().get("http://www.elcats.ru/mercedes/");
 
         getDriver().findElement(By.id("cphMasterPage_txbVIN")).sendKeys(vinNumber);
