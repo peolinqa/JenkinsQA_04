7b5da9187ba8fa873d2a3590fa304b8aa15ba32f
JuliaPrague <yuliach9@gmail.com>
2022-05-16 15:14:19 +0200

    "juliaChTestWocabee"

---
+++ src/test/java/runner/JuliaChTest.java
@@ -0,0 +1,32 @@
+package runner;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class JuliaChTest extends BaseTest {
+
+    @BeforeMethod
+    public void testBefore() {
+        getDriver().get("https://www.wocabee.app/");
+        getDriver().manage().deleteAllCookies();
+    }
+
+    @Test
+    public void testJuliaChWocabeeOpenPage() throws InterruptedException {
+
+        WebElement wocabeeOpenPage = getDriver().findElement(By.xpath("//*[@id=\"page-top\"]"));
+        wocabeeOpenPage.click();
+        Thread.sleep(500);
+
+        WebElement searchButton = getDriver().findElement(By.xpath("//*[@id=\"page-top\"]/header/div/div[2]/div[1]/div/a[1]/div"));
+        Thread.sleep(1000);
+        searchButton.click();
+
+       String text = getDriver().findElement(By.xpath("//*[@id=\"trialModalLabel\"]")).getText();
+        Thread.sleep(1000);
+       Assert.assertEquals(text, "Zvolte typ objednávky");
+    }
+}
