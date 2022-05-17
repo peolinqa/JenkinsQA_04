0a3890874ae637901b5878ebe0cb62e60bc15e44
Anna Fedorova <snysmymric@gmail.com>
2022-05-16 23:49:34 -0700

    Ignore testSearchInputHasKeyAfterSearch()

--- src/test/java/MarinaTest.java
+++ src/test/java/MarinaTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -9,6 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MarinaTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testSearchInputHasKeyAfterSearch() {
         String searchKey = "corolla";
