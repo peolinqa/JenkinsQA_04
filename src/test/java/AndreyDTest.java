470e40bb5aacf58704e213d86bf41cd53ade3e2d
Andrey S <t9326044855@gmail.com>
2022-05-16 19:44:36 -0600

    second commit

--- src/test/java/AndreyDTest.java
+++ src/test/java/AndreyDTest.java
@@ -12,7 +12,7 @@ public class AndreyDTest extends BaseTest {
         getDriver().get("https://ebay.com");
         getDriver().findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("coffee maker");
         getDriver().findElement(By.xpath("//*[@id=\"gh-btn\"]")).click();
-        String res = getDriver().findElement(By.xpath("/html/body/div[5]/div[5]/div[2]/div[1]/div[2]/ul/li[1]/div/div[2]/a/h3\n")).getText();
+        String res = getDriver().findElement(By.xpath("//*[contains(text(),'Keurig K130 B130 In Room Brewing System Coffee Maker')]")).getText();
         System.out.println("res:");
         System.out.println(res);
         Assert.assertEquals(1,1);
