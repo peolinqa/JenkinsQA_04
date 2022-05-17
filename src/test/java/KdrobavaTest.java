1b759b13f8f56ebef83b92d11345fa4c87280d7e
Anna <103545947+AnnaNezhevets@users.noreply.github.com>
2022-05-17 21:48:35 +0300

    First commit added two methods (#150)
    * First commit added two methods
    
    * added ignor to automationpracticeTest

--- src/test/java/KdrobavaTest.java
+++ src/test/java/KdrobavaTest.java
@@ -1,10 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class KdrobavaTest extends BaseTest{
-
+        @Ignore
         @Test
         public void automationpracticeTest () throws InterruptedException {
             getDriver().get("https://automationpractice.com");
