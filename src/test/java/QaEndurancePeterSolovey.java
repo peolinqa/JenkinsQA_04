befa1c54f8f20ca74d09212e8ee6cf7bc87849b0
Solovey-PO <solovey.peter@gmail.com>
2022-05-17 19:49:33 -0600

    Extra changes to my code

--- src/test/java/QaEndurancePeterSolovey.java
+++ src/test/java/QaEndurancePeterSolovey.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import runner.BaseTest;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public class QaEndurancePeterSolovey {
+public class QaEndurancePeterSolovey extends BaseTest {
     String firstName = "Peter";
     String middleName = "O";
     String lastName = "Solovey";
@@ -36,75 +38,73 @@ public class QaEndurancePeterSolovey {
 
     @Test
     public void PeterS2() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-        WebDriver d = new ChromeDriver();
-        d.get("https://skryabin.com/market/quote.html");
+        /*System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+
+        getDriver().get("https://skryabin.com/market/quote.html");
         Dimension dim = new Dimension(1920, 1080);
-        d.manage().window().setSize(dim);
+        getDriver().manage().window().setSize(dim);*/
         DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
         Date date = new Date();
         String dateF = dateFormat.format(date);
         //WebElement currentDate = d.findElement(By.id("currentDate"));
-        Assert.assertEquals(d.findElement(By.id("currentDate")).getText(), dateF);
+        Assert.assertEquals(getDriver().findElement(By.id("currentDate")).getText(), dateF);
 
-        d.findElement(By.id("formSubmit")).click();
-        Assert.assertEquals(d.findElement(By.id("name-error")).getText(), "This field is required.");
+        getDriver().findElement(By.id("formSubmit")).click();
+        Assert.assertEquals(getDriver().findElement(By.id("name-error")).getText(), "This field is required.");
 
-        d.findElement(By.name("name")).click();
-        WebElement firstNameE = d.findElement(By.name("firstName"));
+        getDriver().findElement(By.name("name")).click();
+        WebElement firstNameE = getDriver().findElement(By.name("firstName"));
         firstNameE.sendKeys(firstName);
-        WebElement middleNameE = d.findElement(By.name("middleName"));
+        WebElement middleNameE = getDriver().findElement(By.name("middleName"));
         middleNameE.sendKeys(middleName);
-        WebElement lastNameE = d.findElement(By.name("lastName"));
+        WebElement lastNameE = getDriver().findElement(By.name("lastName"));
         lastNameE.sendKeys(lastName);
-        d.findElement(By.xpath("//span[text() = 'Save']")).click();
+        getDriver().findElement(By.xpath("//span[text() = 'Save']")).click();
         Thread.sleep(500);
 
         /*String value = d.findElement(By.id("name")).getAttribute("value");
         System.out.println(value);
         Assert.assertEquals(d.findElement(By.name("name")).getAttribute("value"), "Peter O Solovey");*/
 
-        d.findElement(By.name("username")).sendKeys("1");
-        d.findElement(By.id("formSubmit")).click();
-        Assert.assertEquals(d.findElement(By.id("username-error")).getText(), "Please enter at least 2 characters.");
+        getDriver().findElement(By.name("username")).sendKeys("1");
+        getDriver().findElement(By.id("formSubmit")).click();
+        Assert.assertEquals(getDriver().findElement(By.id("username-error")).getText(), "Please enter at least 2 characters.");
         //d.findElement(By.name("username")).sendKeys("PS123451645");
-        d.quit();
+        getDriver().quit();
     }
 
     @Test
     public void PeterS3() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-        WebDriver d = new ChromeDriver();
-        d.get("https://skryabin.com/market/quote.html");
-        d.findElement(By.name("name")).click();
-        WebElement firstNameE = d.findElement(By.name("firstName"));
+        getDriver().get("https://skryabin.com/market/quote.html");
+        getDriver().findElement(By.name("name")).click();
+        WebElement firstNameE = getDriver().findElement(By.name("firstName"));
         firstNameE.sendKeys(firstName);
-        WebElement middleNameE = d.findElement(By.name("middleName"));
+        WebElement middleNameE = getDriver().findElement(By.name("middleName"));
         middleNameE.sendKeys(middleName);
-        WebElement lastNameE = d.findElement(By.name("lastName"));
+        WebElement lastNameE = getDriver().findElement(By.name("lastName"));
         lastNameE.sendKeys(lastName);
-        d.findElement(By.xpath("//span[text() = 'Save']")).click();
+        getDriver().findElement(By.xpath("//span[text() = 'Save']")).click();
         Thread.sleep(500);
-        d.findElement(By.name("username")).sendKeys("Peter12345");
-        d.findElement(By.name("email")).sendKeys("12345@d.com");
-        d.findElement(By.name("password")).sendKeys("123456");
-        d.findElement(By.name("confirmPassword")).sendKeys("123456");
+        getDriver().findElement(By.name("username")).sendKeys("Peter12345");
+        getDriver().findElement(By.name("email")).sendKeys("12345@d.com");
+        getDriver().findElement(By.name("password")).sendKeys("123456");
+        getDriver().findElement(By.name("confirmPassword")).sendKeys("123456");
 
 
-        Select selectCountry = new Select(d.findElement(By.name("countryOfOrigin")));
+        Select selectCountry = new Select(getDriver().findElement(By.name("countryOfOrigin")));
         selectCountry.selectByValue("Belarus");
-        d.findElement(By.xpath("//input[@value = 'male']")).click();
+        getDriver().findElement(By.xpath("//input[@value = 'male']")).click();
 
-        d.findElement(By.name("dateOfBirth")).click();
-        Select selectMonth = new Select(d.findElement(By.className("ui-datepicker-month")));
+        getDriver().findElement(By.name("dateOfBirth")).click();
+        Select selectMonth = new Select(getDriver().findElement(By.className("ui-datepicker-month")));
         selectMonth.selectByValue("4");
-        Select selectYear = new Select(d.findElement(By.className("ui-datepicker-year")));
+        Select selectYear = new Select(getDriver().findElement(By.className("ui-datepicker-year")));
         selectYear.selectByValue("2020");
-        d.findElement(By.xpath("//table[@class = 'ui-datepicker-calendar']//a[text() = '20']")).click();
-        d.findElement(By.name("allowedToContact")).click();
-        d.findElement(By.id("address")).sendKeys("alskjdsngakjsngkjasgnksajgnaskghnaskuvnzdkjbndkjflgnsdkfn");
+        getDriver().findElement(By.xpath("//table[@class = 'ui-datepicker-calendar']//a[text() = '20']")).click();
+        getDriver().findElement(By.name("allowedToContact")).click();
+        getDriver().findElement(By.id("address")).sendKeys("alskjdsngakjsngkjasgnksajgnaskghnaskuvnzdkjbndkjflgnsdkfn");
 
-        Select selectCar = new Select(d.findElement(By.name("carMake")));
+        Select selectCar = new Select(getDriver().findElement(By.name("carMake")));
         if (selectCar.isMultiple()) {
             selectCar.selectByValue("Ford");
             selectCar.selectByValue("Toyota");
@@ -112,19 +112,19 @@ public class QaEndurancePeterSolovey {
         }
 
         //iframe
-        d.switchTo().frame("additionalInfo");
-        d.findElement(By.id("contactPersonName")).sendKeys("XXX");
-        d.findElement(By.id("contactPersonPhone")).sendKeys("123456789");
-        d.switchTo().parentFrame();
-        d.findElement(By.name("agreedToPrivacyPolicy")).click();
+        getDriver().switchTo().frame("additionalInfo");
+        getDriver().findElement(By.id("contactPersonName")).sendKeys("XXX");
+        getDriver().findElement(By.id("contactPersonPhone")).sendKeys("123456789");
+        getDriver().switchTo().parentFrame();
+        getDriver().findElement(By.name("agreedToPrivacyPolicy")).click();
 
-        int countErrors = d.findElements(By.className("error")).size();
+        int countErrors = getDriver().findElements(By.className("error")).size();
         System.out.println(countErrors);
 
-        d.findElement(By.id("formSubmit")).click();
+        getDriver().findElement(By.id("formSubmit")).click();
 
         Thread.sleep(3000);
-        d.quit();
+        getDriver().quit();
 
     }
 
