f3844db30d8f96e86e1b75cb11de2c586b929ebd
Victor <victor.goltsov@gmail.com>
2022-05-21 11:08:20 +0300

    First commit with 2 methods (#207)

---
+++ src/test/java/VicNoobisTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class VicNoobisTest extends BaseTest {
+    public static final String URL ="http://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testOneProgramInTittle () {
+        getDriver().get(URL);
+        WebElement text = getDriver().findElement(By.xpath("//div[@id='header']/h2/strong"));
+
+        String expectedResult = "1500";
+        String actualResult = text.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testFindAuthorOfZombie () {
+        getDriver().get(URL);
+
+        WebElement topList = getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href='/toplist.html']"));
+
+        topList.click();
+        WebElement zombie = getDriver().findElement(By.xpath("//table[@id='category']//a[@href='language-zombie-2562.html']"));
+
+        zombie.click();
+
+        WebElement autor = getDriver().findElement(By.xpath("//div[@id='main']//td[(contains(text(),'Taneb'))]"));
+
+        String expectedResult = "Taneb";
+        String actualResult = autor.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+}
