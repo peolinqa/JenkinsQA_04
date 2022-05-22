63f7080bbedc36f398d1fcf55dc647cb4c907eec
maratdp <34253526+maratdp@users.noreply.github.com>
2022-05-22 23:02:20 +0300

    maratdp/add fixes after common review session (#243)

--- src/test/java/MaratTest.java
+++ src/test/java/MaratTest.java
@@ -9,64 +9,55 @@ import java.util.List;
 
 public class MaratTest extends BaseTest {
 
-    private void getWikiStartPage() {
-        getDriver().get("https://www.wikipedia.org/");
-    }
+    private static final String URL = "https://www.wikipedia.org/";
 
     private void initiateSearch(String request) {
         getDriver().findElement(By.id("searchInput")).sendKeys(request);
-        getDriver().findElement(By.xpath("//*[@class='sprite svg-search-icon']")).submit();
-    }
-
-    @Test
-    public void testStartPageTitle() {
-        getWikiStartPage();
-        Assert.assertEquals(getDriver().getTitle(), "Wikipedia");
+        getDriver().findElement(By.xpath("//i[@class='sprite svg-search-icon']")).submit();
     }
 
     @Test
     public void testLanguageChange() {
-        getWikiStartPage();
+        getDriver().get(URL);
 
         Select languageSelector = new Select(getDriver().findElement(By.id("searchLanguage")));
         languageSelector.selectByVisibleText("Italiano");
 
         WebElement languageIcon = getDriver().findElement(By.id("jsLangLabel"));
-
-        Assert.assertEquals(languageIcon.getText().toLowerCase(), "it".toLowerCase());
+        Assert.assertEquals(languageIcon.getText(), "IT");
     }
 
     @Test
     public void testSearchResult() {
-        getWikiStartPage();
+        getDriver().get(URL);
         initiateSearch("Test automation");
 
         WebElement testAutomationHeader = getDriver().findElement(By.id("firstHeading"));
         Assert.assertEquals(testAutomationHeader.getText(), "Test automation");
-
     }
 
     @Test
     public void testTableOfContents() {
-        getWikiStartPage();
+        getDriver().get(URL);
         initiateSearch("Test automation");
 
-        List<WebElement> contents = getDriver().findElements(By.xpath("//*[@id='toc']//ul/li[contains(@class,'toclevel-1')]"));
+        List<WebElement> contents = getDriver().findElements(
+                By.xpath("//li[contains(@class,'toclevel-1')]"));
         Assert.assertEquals(contents.size(), 10);
 
-        WebElement contentsItem = getDriver().findElement(By
-                .xpath("//*[@id='toc']//a[contains(@href,'Framework')]/span[@class='toctext']"));
+        WebElement contentsItem = getDriver().findElement(
+                By.xpath("//a[contains(@href,'Framework')]/span[@class='toctext']"));
         Assert.assertEquals(contentsItem.getText(), "Framework approach in automation");
     }
 
     @Test
     public void testWikiMainPage() {
-        getWikiStartPage();
+        getDriver().get(URL);
         initiateSearch("Manual testing");
 
         getDriver().findElement(By.className("mw-wiki-logo")).click();
-
-        WebElement wikiHeadline = getDriver().findElement(By.id("Welcome_to_Wikipedia"));
-        Assert.assertEquals(wikiHeadline.getText(), "Welcome to Wikipedia");
+        Assert.assertEquals(getDriver().findElement(By.id("Welcome_to_Wikipedia")).getText(),
+                "Welcome to Wikipedia");
     }
 }
+
