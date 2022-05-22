cfcd1d0a9738a78dad8ab6b38f2cc880ae968c88
pojirakula <pojirakula@gmail.com>
2022-05-22 11:37:34 -0700

    xpath changed

--- src/test/java/OlaTest.java
+++ src/test/java/OlaTest.java
@@ -7,9 +7,10 @@ import runner.BaseTest;
 
 public class OlaTest extends BaseTest  {
     @Test
-    public void testMvnRepositoryHomePageFindText() {
+    public void testMvnRepositoryHomePageFindText() throws InterruptedException {
 
         getDriver().get("https://mvnrepository.com/");
+        Thread.sleep(2000);
         WebElement h1 = getDriver().findElement(By.xpath("//div[@id='maincontent']/h1"));
         String actualResult = h1.getText();
 
