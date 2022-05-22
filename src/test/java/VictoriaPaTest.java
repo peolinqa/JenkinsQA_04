594257743d269900dfdcac4d07e0f54998c3d36f
VictoriaP <victoriaparfenovich@gmail.com>
2022-05-21 17:17:30 -0700

    new branch ViPa

---
+++ src/test/java/VictoriaPaTest.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class VictoriaPaTest extends BaseTest {
+
+        @Test
+        public void testItemInCard() throws InterruptedException {
+
+            getDriver().get("https://www.saucedemo.com/");
+            getDriver().findElement(By.id("user-name")).sendKeys("standard_user");
+            getDriver().findElement(By.id("password")).sendKeys("secret_sauce");
+            getDriver().findElement(By.id("login-button")).click();
+
+            getDriver().findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+            getDriver().findElement(By.xpath("//span[@class='shopping_cart_badge']")).click();
+
+            Assert.assertTrue(getDriver().getCurrentUrl().contains("https://www.saucedemo.com/cart.html"));
+            getDriver().quit();
+        }
+    }
+
