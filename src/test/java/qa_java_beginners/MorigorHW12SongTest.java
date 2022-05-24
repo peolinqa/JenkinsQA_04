1e54e728fbe2987b4e2abc0e1bd86eafe3417214
Morigor <103515819+Morigor@users.noreply.github.com>
2022-05-24 23:50:44 +0300

    class MorigorHW12SongTest created, testAreLanguagesSortedByLetter() added (#294)

---
+++ src/test/java/qa_java_beginners/MorigorHW12SongTest.java
@@ -0,0 +1,29 @@
+package qa_java_beginners;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class MorigorHW12SongTest extends BaseTest {
+
+    @Test
+    public void testAreLanguagesSortedByLetter() {
+        String expectedResult
+                = "All languages starting with the letter J are shown, sorted by Language.";
+        getDriver().get("http://www.99-bottles-of-beer.net/");
+
+        getDriver().findElement(
+                By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']")
+        ).click();
+        getDriver().findElement(
+                By.xpath("//ul[@id='submenu']/li/a[@href='j.html']")
+        ).click();
+
+        String actualResult = getDriver()
+                .findElement(By.xpath("//div[@id='main']/p[text()]")).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+}
