c6e205898325ad6951d4184990cd1e6b1c5ef0c4
TatsianaZarankina <tatsiana.zarankina@gmail.com>
2022-05-24 17:37:06 -0400

    New Test

--- src/test/java/TZGroupBugHuntersTest.java
+++ src/test/java/TZGroupBugHuntersTest.java
@@ -1,5 +1,6 @@
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -7,20 +8,49 @@ import org.testng.annotations.Test;
 import runner.BaseTest;
 
 
+
 public class TZGroupBugHuntersTest extends BaseTest {
-    @Ignore
+
     @Test
 
-    public void tatsianaZarankinaFirstTest() throws InterruptedException {
-        getDriver().get("https://www.reddit.com/");
+    public void testTatsianaZarMoto() {
+        getDriver().get("https://rifle.com/Default.aspx");
 
 
-        WebElement searchBox = getDriver().findElement(By.xpath("//*[@id=\"header-search-bar\"]"));
-        searchBox.sendKeys("Java");
-        WebElement searchButton = getDriver().findElement(By.xpath("//*[@id=\"SearchDropdownContent\"]/a[6]/i"));
-        searchButton.click();
-        Thread.sleep(1000);
-        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.reddit.com/search/?q=Java");
+        WebElement fairings = getDriver().findElement(By.xpath("//a[@href='Motorcycle-Fairings/Rifle-Fairings.aspx']"));
+        fairings.click();
+
+        WebElement rifleRavenFairing = getDriver().findElement(By.xpath("//a[@href='Rifle-Raven-Fairing.aspx']"));
+        rifleRavenFairing.click();
+
+        WebElement chooseOption = getDriver().findElement(
+                By.xpath("//a[@href='Honda/Raven-Fairing-Honda-Valkyrie-VTX1800-C-R.aspx']"));
+        chooseOption.click();
+
+
+        Assert.assertEquals(getDriver().getCurrentUrl(),
+                "https://rifle.com/Motorcycle-Fairings/Honda/Raven-Fairing-Honda-Valkyrie-VTX1800-C-R.aspx");
 
     }
+
+    @Test
+    public void testTatsianaZarContact() {
+        getDriver().get("https://rifle.com/Default.aspx");
+        WebElement contactInformation = getDriver().findElement(
+                By.xpath("//a[@id='ctl00_ctl00_NestedMaster_PageHeader_StoreHeaderRifle_H_BootNavContactLink']")
+        );
+        contactInformation.click();
+        WebElement customerService = getDriver().findElement(
+                By.xpath("//a[@href='/Rifle-Customer-Service-Email.aspx?s=cs']")
+        );
+        customerService.click();
+
+        Assert.assertEquals(getDriver().getCurrentUrl(),
+                "https://rifle.com/Rifle-Customer-Service-Email.aspx?s=cs");
+
+    }
+
 }
+
+
+
