a986bc9e6a5b60aeac9a9382a5e8d9f6fe96b80c
Tatiana P <103320248+dmswlpk@users.noreply.github.com>
2022-05-23 22:46:32 -0600

    HW12_1test added (#273)

--- src/test/java/qa_java_beginners/Song99BottlesTatianaPTest.java
+++ src/test/java/qa_java_beginners/Song99BottlesTatianaPTest.java
@@ -1,6 +1,7 @@
 package qa_java_beginners;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -133,5 +134,18 @@ public class Song99BottlesTatianaPTest extends BaseTest {
         }
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+
+    public void HW_12_1test(){
+
+        String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/");
+        getDriver().findElement(By.xpath("//li/a[@href='/abc.html']")).click();
+        getDriver().findElement(By.xpath("//li/a[@href='j.html']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main']/p[1]")).getText(), expectedResult);
     }
 }
