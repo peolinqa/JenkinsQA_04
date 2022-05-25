a4c37145cbcca9c71a53f4430329f33a60f762aa
Irina Sh <104162505+Kononirin@users.noreply.github.com>
2022-05-25 23:13:32 +0500

    Add TC_12_02 (#319)

--- src/test/java/qa_java_beginners/HW12KononirinIrinaShTest.java
+++ src/test/java/qa_java_beginners/HW12KononirinIrinaShTest.java
@@ -1,6 +1,9 @@
 package qa_java_beginners;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -10,8 +13,8 @@ public class HW12KononirinIrinaShTest extends BaseTest {
     @Test
     public void testTitleSubmenuByJ() {
 
-        String expectedResult = "All languages starting with the letter J are shown, "
-                + "sorted by Language.";
+        String expectedResult = "All languages starting with the letter J "
+                + "are shown, sorted by Language.";
 
         getDriver().get("http://www.99-bottles-of-beer.net/");
 
@@ -28,7 +31,33 @@ public class HW12KononirinIrinaShTest extends BaseTest {
         String actualResult =
                 getDriver()
                         .findElement(
-                                By.xpath("//div[@id='main']/p")).getText();
+                                By.xpath("//div[@id='main']/p"))
+                        .getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testLastLangMySQL() {
+
+        String expectedResult = "MySQL";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/");
+
+        WebElement menuBrowseLang = getDriver()
+                .findElement(
+                        By.xpath("//li/a[@href='/abc.html']"));
+        menuBrowseLang.click();
+
+        WebElement subMenuM = getDriver()
+                .findElement(
+                        By.xpath("//a[@href='m.html']"));
+        subMenuM.click();
+
+        WebElement lastLangMySQL = getDriver()
+                .findElement(By.xpath("//tr[last()]/td/a"));
+
+        String actualResult = lastLangMySQL.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
     }
