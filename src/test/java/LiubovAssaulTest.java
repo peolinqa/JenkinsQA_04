6dd971d6840fcd871c6dbb91f4c19bbd5511e1da
lassaul <104651012+lassaul@users.noreply.github.com>
2022-05-25 09:47:29 +0300

    refactor: testAddToCartOneItemFromInventoryPage, add: testAddToCartOneItemFromItemPage, testRemoveItemFromCart (#303)

--- src/test/java/LiubovAssaulTest.java
+++ src/test/java/LiubovAssaulTest.java
@@ -1,38 +1,66 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class LiubovAssaulTest extends BaseTest {
 
+    public static final String URL = "https://www.saucedemo.com/";
+    public static final String USERNAME = "standard_user";
+    public static final String PASSWORD = "secret_sauce";
+
     @Test
-    public void addToCartOneItem () {
+    public void testAddToCartOneItemFromInventoryPage() {
+        getDriver().get(URL);
 
-        getDriver().get("https://www.saucedemo.com/");
-
-        String title = getDriver().getTitle();
-        Assert.assertEquals (title, "Swag Labs");
-
-        WebElement userName = getDriver().findElement(By.id("user-name"));
-        WebElement password = getDriver().findElement(By.id("password"));
-        WebElement logButton = getDriver().findElement(By.id("login-button"));
-
-        userName.sendKeys("standard_user");
-        password.sendKeys("secret_sauce");
-        logButton.click();
-
-        String menuTitle = getDriver().findElement(By.xpath("//span[@class = 'title']")).getText();
-        Assert.assertEquals(menuTitle, "PRODUCTS");
+        getDriver().findElement(By.id("user-name")).sendKeys(USERNAME);
+        getDriver().findElement(By.id("password")).sendKeys(PASSWORD);
+        getDriver().findElement(By.id("login-button")).click();
 
         getDriver().findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
         getDriver().findElement(By.id("remove-sauce-labs-backpack")).isDisplayed();
 
-        String itemQuantity = getDriver().findElement(By.cssSelector(".shopping_cart_badge")).getText();
-        Assert.assertEquals(itemQuantity, "1");
+        String actualResult = getDriver().findElement(By.cssSelector(".shopping_cart_badge")).getText();
 
+        Assert.assertEquals(actualResult, "1");
     }
 
+    @Test
+    public void testAddToCartOneItemFromItemPage() {
+        getDriver().get(URL);
+
+        getDriver().findElement(By.id("user-name")).sendKeys(USERNAME);
+        getDriver().findElement(By.id("password")).sendKeys(PASSWORD);
+        getDriver().findElement(By.id("login-button")).click();
+
+        getDriver().findElement(
+                By.xpath("//div[@class='inventory_item_name' and text()='Sauce Labs Backpack']")
+        ).click();
+        getDriver().findElement(By.id("back-to-products")).isDisplayed();
+
+        getDriver().findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
+        getDriver().findElement(By.id("remove-sauce-labs-backpack")).isDisplayed();
+
+        String actualResult = getDriver().findElement(By.cssSelector(".shopping_cart_badge")).getText();
+
+        Assert.assertEquals(actualResult, "1");
+    }
+
+    @Test
+    public void testRemoveItemFromCart() {
+        getDriver().get(URL);
+
+        getDriver().findElement(By.id("user-name")).sendKeys(USERNAME);
+        getDriver().findElement(By.id("password")).sendKeys(PASSWORD);
+        getDriver().findElement(By.id("login-button")).click();
+
+        getDriver().findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        getDriver().findElement(By.xpath("//span[@class ='shopping_cart_badge'][text()='1']")).isDisplayed();
+
+        getDriver().findElement(By.id("remove-sauce-labs-backpack")).click();
+
+        String actualResult = getDriver().findElement(By.cssSelector(".shopping_cart_link")).getText();
+
+        Assert.assertEquals(actualResult, "");
+    }
 }
