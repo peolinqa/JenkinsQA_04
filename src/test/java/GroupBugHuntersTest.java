b76725c8cbcd01ecd5d371eae3048b34665f2ce2
BOksanaV <BOV1702@gmail.com>
2022-05-24 21:17:37 -0400

    ignore testOksanaBakatova

--- src/test/java/GroupBugHuntersTest.java
+++ src/test/java/GroupBugHuntersTest.java
@@ -1,11 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class GroupBugHuntersTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testOksanaBakatova() throws InterruptedException {
         getDriver().get("https://www.etsy.com");
