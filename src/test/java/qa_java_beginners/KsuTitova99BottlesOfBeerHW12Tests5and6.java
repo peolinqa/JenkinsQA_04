e7d63f3f8b76a34a40044901eadf0cab9db94a3b
ksutitova <103765645+ksutitova@users.noreply.github.com>
2022-05-26 16:21:56 +0300

    testSecurityCodeField (#348)

--- src/test/java/qa_java_beginners/KsuTitova99BottlesOfBeerHW12Tests5and6.java
+++ src/test/java/qa_java_beginners/KsuTitova99BottlesOfBeerHW12Tests5and6.java
@@ -29,4 +29,44 @@ public class KsuTitova99BottlesOfBeerHW12Tests5and6 extends BaseTest {
 
         Assert.assertEquals(actualResult.length, expectedResult);
     }
+
+
+    /**
+     * TC_12_06 Подтвердите, что если на странице Sign Guestbook
+     * http://www.99-bottles-of-beer.net/signv2.html вы заполните все поля формы,
+     * но введете случайно сгенерированное трехзначное число в поле  Security Code: ,
+     * то вы получите сообщение об ошибке  Error: Error: Invalid security code.
+     */
+
+    @Test
+    public void testSecurityCodeField() {
+        String expectedResult = "Error: Error: Invalid security code.";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/signv2.html");
+
+        getDriver().findElement(By.xpath
+                ("//form/p/input[@name='name']")).sendKeys("Ksenia");
+        getDriver().findElement(By.xpath
+                ("//form/p/input[@name='location']")).sendKeys("Sankt-Peterburg");
+        getDriver().findElement(By.xpath
+                ("//form/p/input[@name='email']")).sendKeys("ksunchik123@yandex.ru");
+        getDriver().findElement(By.xpath
+                ("//form/p/input[@name='homepage']")).sendKeys("https://www.google.com/");
+        getDriver().findElement(By.xpath("//form/p/input[@name='captcha']")).sendKeys
+                (Integer.toString((int) (Math.random() * 1000 + 100)));
+        getDriver().findElement(By.xpath("//textarea[@name='comment']")).sendKeys
+                ("Test");
+
+        getDriver().findElement(By.xpath("//input[@name='submit']")).click();
+
+        String actualResult = getDriver().findElement(By.xpath("//div[@id='main']/p")).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+
+
+
+
+
 }
