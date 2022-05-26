3dc696e0ed29bd7b553b883bc2920a85c59a1f16
Max <100244529+MaksPt@users.noreply.github.com>
2022-05-27 00:10:39 +0300

    MaksPt/HW12TC6 created. (#367)

--- src/test/java/qa_java_beginners/MaksPtHomeWork12Test.java
+++ src/test/java/qa_java_beginners/MaksPtHomeWork12Test.java
@@ -38,8 +38,8 @@ public class MaksPtHomeWork12Test extends BaseTest {
         searchBrowseLanguages().click();
         searchSubmenuMLanguages().click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='language-mysql-1252.html']")).getText(),
-                "MySQL");
+        Assert.assertEquals(getDriver()
+                        .findElement(By.xpath("//a[@href='language-mysql-1252.html']")).getText(),"MySQL");
     }
 
     @Test
@@ -72,7 +72,28 @@ public class MaksPtHomeWork12Test extends BaseTest {
         searchBrowseLanguages().click();
         getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='0.html']")).click();
 
-        Assert.assertEquals(getDriver().findElements(By.xpath("//table[@id='category']/tbody/tr[@onmouseover]")).size(),
-                10);
+        Assert.assertEquals(getDriver()
+                .findElements(By.xpath("//table[@id='category']/tbody/tr[@onmouseover]")).size(),10);
+    }
+
+    @Test
+    public void testGuestBookSecurityCodeError() {
+        getDriver().get("http://www.99-bottles-of-beer.net/signv2.html");
+
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys("Max");
+        getDriver().findElement(By.xpath("//input[@name='location']")).sendKeys("Rostov");
+        getDriver().findElement(By.xpath("//input[@name='email']")).sendKeys("rostov@aaanet.com");
+        getDriver().findElement(By.xpath("//input[@name='homepage']")).sendKeys("rostov.com");
+        getDriver().findElement(By.xpath("//input[@name='captcha']"))
+                .sendKeys(Integer.toString((int) (Math.random() * 900 + 100)));
+        getDriver().findElement(By.xpath("//input[@name='homepage']")).sendKeys("rostov.com");
+        getDriver().findElement(By.xpath("//textarea[@name='comment']")).sendKeys("Hello World!");
+        getDriver().findElement(By.xpath("//input[@name='submit']")).click();
+
+        Assert.assertEquals(getDriver()
+                        .findElement(By.xpath("//div[@id='main']/p")).getText(),"Error: Error: Invalid security code.");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main']/p/b")).getTagName(),"b");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='main']/p")).getAttribute("style"),
+                "border: 1px solid red; background-color: rgb(255, 224, 224); padding: 5px; margin: 5px 10px;");
     }
 }
