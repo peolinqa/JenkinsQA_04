cd984d8fe93dff8d559cb384d53f31850b49a569
Max <100244529+MaksPt@users.noreply.github.com>
2022-05-26 18:35:55 +0300

    MaksPtHomeWork12Test3_4_5 created. (#351)
    * HW12TC3_4_5 created.

--- src/test/java/qa_java_beginners/MaksPtHomeWork12Test.java
+++ src/test/java/qa_java_beginners/MaksPtHomeWork12Test.java
@@ -15,6 +15,11 @@ public class MaksPtHomeWork12Test extends BaseTest {
         return getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']"));
     }
 
+    private WebElement searchSubmenuMLanguages() {
+
+        return getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='m.html']"));
+    }
+
     @Test
     public void testLanguagesTextJ() {
         getDriver().get(URL);
@@ -31,9 +36,43 @@ public class MaksPtHomeWork12Test extends BaseTest {
         getDriver().get(URL);
 
         searchBrowseLanguages().click();
-        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='m.html']")).click();
+        searchSubmenuMLanguages().click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='language-mysql-1252.html']")).getText(),
                 "MySQL");
+    }
+
+    @Test
+    public void testLanguagesTableHeader() {
+        final String expectedResult = "Language, Author, Date, Comments, Rate";
+
+        getDriver().get(URL);
+
+        searchBrowseLanguages().click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//table[@id ='category']/tbody/tr[1]")).getText(),
+                expectedResult.replace(",", ""));
+    }
+
+    @Test
+    public void testMathematicaLanguage() {
+        getDriver().get(URL);
+
+        searchBrowseLanguages().click();
+        searchSubmenuMLanguages().click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='language-mathematica-1090.html']/../..")).getText(),
+                "Mathematica Brenton Bostick 03/16/06 1");
+    }
+
+    @Test
+    public void testNamesStartNumbersLanguages() {
+        getDriver().get(URL);
+
+        searchBrowseLanguages().click();
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='0.html']")).click();
+
+        Assert.assertEquals(getDriver().findElements(By.xpath("//table[@id='category']/tbody/tr[@onmouseover]")).size(),
+                10);
     }
 }
