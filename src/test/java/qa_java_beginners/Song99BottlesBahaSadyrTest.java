08d14f0a89bd384a7743adf742b9d982574c77ae
bahasadyr <105130043+bahasadyr@users.noreply.github.com>
2022-05-27 06:57:13 -0500

    Baha sadyr (#378)
    * added HW12 TC 12_01
    
    * added test case HW12 TC_03
    
    * added test case HW12 TC_03

--- src/test/java/qa_java_beginners/Song99BottlesBahaSadyrTest.java
+++ src/test/java/qa_java_beginners/Song99BottlesBahaSadyrTest.java
@@ -6,6 +6,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Song99BottlesBahaSadyrTest extends BaseTest {
 
     public static final String BASE_URL = "http://www.99-bottles-of-beer.net/";
@@ -20,6 +24,30 @@ public class Song99BottlesBahaSadyrTest extends BaseTest {
         String actualRes = getDriver().findElement(By.xpath("//p[contains(text(),'All languages starting with the letter')]")).getText().trim();
 
         Assert.assertEquals(actualRes, expectedRes);
+    }
+    @Test
+    public void test_TC_12_02(){
+
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath("//a[contains(text(),'Browse Languages')][1]")).click();
+        getDriver().findElement(By.xpath("//a[@href='m.html']")).click();
+        String actualRes = getDriver().findElement(By.xpath("//table[@id='category']//tr[last()]//td//a")).getText();
+
+        Assert.assertEquals(actualRes,"MySQL");
+    }
+    @Test
+    public void test_TC_12_03(){
+
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath("//a[contains(text(),'Browse Languages')][1]")).click();
+        List<WebElement> listWithHeaders = getDriver().findElements(By.xpath("//table[@id='category']//tr//th"));
+        List<String>actualRes = new ArrayList<>();
+        for(int i = 0; i < listWithHeaders.size(); i++){
+            actualRes.add(listWithHeaders.get(i).getText());
+        }
+        List<String>expectRes = Arrays.asList("Language", "Author", "Date", "Comments", "Rate");
+
+        Assert.assertEquals(actualRes,expectRes);
     }
 
 }
