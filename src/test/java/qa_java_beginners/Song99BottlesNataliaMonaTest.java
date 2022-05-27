3451a8e1843954cd5972135b5798e88e3f15b5b7
Natalia <104234652+NataliaMona@users.noreply.github.com>
2022-05-27 16:17:43 -0400

    Added two tests tests (#388)
    * Added two tests testConfirmTheLastProgrammingLanguage() and
    testConfirmTheTableWithHeaders()
    
    * Added to Ignore testCheckTablet() in the class LetsBeTestersTest.java (Vitaly Zverev)
    
    * 'm re-launching it for verification

--- src/test/java/qa_java_beginners/Song99BottlesNataliaMonaTest.java
+++ src/test/java/qa_java_beginners/Song99BottlesNataliaMonaTest.java
@@ -6,10 +6,10 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-public class Song99BottlesNataliaMonaTest extends BaseTest {
+public class Song99BottlesNataliaMonaTest extends BaseTest{
 
     @Test
-    public void testConfirmThePresenceOfASubmenuJInTheMenuBrowseLanguages() {
+    public void testConfirmThePresenceOfASubmenuJInTheMenuBrowseLanguages(){
 
         String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
 
@@ -22,5 +22,42 @@ public class Song99BottlesNataliaMonaTest extends BaseTest {
         String actualResult = text.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+    }
+    @Test
+    public void testConfirmTheLastProgrammingLanguage(){
+
+        String expectedResult = "MySQL";
+
+        getDriver().get("https://www.99-bottles-of-beer.net/");
+
+        getDriver().findElement(By.xpath(
+                "//div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")).click();
+        getDriver().findElement(By.xpath(
+                "//div[@id='navigation']/ul[@id='submenu']/li/a[@href ='m.html']")).click();
+        WebElement language = getDriver().findElement(By.xpath(
+                "//table[@id='category']/tbody/tr/td/a[@href='language-mysql-1252.html']"));
+        String actualResult = language.getText();
+
+        Assert.assertEquals(actualResult,expectedResult);
+    }
+    @Test
+    public void testConfirmTheTableWithHeaders(){
+
+        String[] expectedResult = {"Language","Author", "Date","Comments","Rate"};
+
+        getDriver().get("https://www.99-bottles-of-beer.net/");
+
+        getDriver().findElement(By.xpath(
+                "//div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")).click();
+        WebElement language = getDriver().findElement(By.xpath(
+                "//div[@id='main']/table/tbody/tr/th[@style='width: 40%;']"));
+        WebElement author = getDriver().findElement(By.xpath(
+                "//div[@id='main']/table/tbody/tr/th[@style='width: 30%;']"));
+        WebElement date = getDriver().findElement(By.xpath("//div[@id='main']/table/tbody/tr/th[3]"));
+        WebElement comments = getDriver().findElement(By.xpath("//div[@id='main']/table/tbody/tr/th[4]"));
+        WebElement rate = getDriver().findElement(By.xpath("//div[@id='main']/table/tbody/tr/th[5]"));
+        String[] actualResult = {language.getText(), author.getText(), date.getText(), comments.getText(), rate.getText()};
+
+        Assert.assertEquals(actualResult,expectedResult);
     }
 }
