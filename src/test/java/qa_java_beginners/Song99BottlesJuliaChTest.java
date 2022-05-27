6dc97f130ae0f480a38acd1c16ffda5be4eb9902
Julia <103532260+JuliaChPrague@users.noreply.github.com>
2022-05-27 21:23:11 +0200

    added test3 (#395)

--- src/test/java/qa_java_beginners/Song99BottlesJuliaChTest.java
+++ src/test/java/qa_java_beginners/Song99BottlesJuliaChTest.java
@@ -1,9 +1,7 @@
 package qa_java_beginners;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
@@ -151,6 +149,7 @@ public class Song99BottlesJuliaChTest extends BaseTest {
         Assert.assertEquals(getDriver().findElement(
                 By.xpath("//div[@id='wrap']/div[@id='main']/p")).getText(), expectedResult);
     }
+
     @Test
     public void testLastLanguageMySQL() {
 
