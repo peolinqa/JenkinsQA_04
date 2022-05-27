ab0cd8d2d41ffd20a1948804e5b22d628e8b28e5
AnaLama <104392942+AnaLama@users.noreply.github.com>
2022-05-27 12:15:54 -0400

    "HW12 task 3-5 added" (#390)

--- src/test/java/qa_java_beginners/AnaLamaHW12Tests.java
+++ src/test/java/qa_java_beginners/AnaLamaHW12Tests.java
@@ -6,45 +6,85 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AnaLamaHW12Tests extends BaseTest {
 
-        @Test
-        public void testBrowseJLanguage(){
-    String baseUrl = "http://www.99-bottles-of-beer.net/";
-    String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
+    private final String BASE_URL = "http://www.99-bottles-of-beer.net/";
+    private final String BROWSE_LANG = "//div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']";
 
-    getDriver().get(baseUrl);
+    @Test
+    public void testBrowseJLanguage() {
 
-    getDriver().findElement(By.xpath(
-            "//div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"))
-            .click();
-    getDriver().findElement(By.xpath(
-            "//ul[@id='submenu']/li/a[@href='j.html']")).click();
+        String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
 
-    WebElement letJ = getDriver().findElement(By.xpath("//div[@id='main']/p"));
-    String actualResult = letJ.getText();
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath(BROWSE_LANG)).click();
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='j.html']")).click();
 
-            Assert.assertEquals(actualResult, expectedResult);
+        WebElement letJ = getDriver().findElement(By.xpath("//div[@id='main']/p"));
+        String actualResult = letJ.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testMLastLanguageMySQL() {
+
+        String expectedResult = "MySQL";
+
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath(BROWSE_LANG)).click();
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='m.html']")).click();
+
+        WebElement mLet = getDriver().findElement(By.xpath("//tr[last()]/td/a"));
+        String actualResult = mLet.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+
+    @Test
+    public void testTableColumnsTitles() {
+
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath(BROWSE_LANG)).click();
+
+        List<WebElement> tabTitles = getDriver().findElements(By.xpath("//table[@id='category']/tbody/tr/th"));
+        List<String> tabTitlesActual = new ArrayList<>();
+        for (int i = 0; i < tabTitles.size(); i++) {
+            tabTitlesActual.add(tabTitles.get(i).getText());
         }
+        List<String> expectedResult = new ArrayList<>(Arrays.asList("Language", "Author", "Date", "Comments", "Rate"));
 
-        @Test
-        public void testMLastLanguageMySQL(){
+        Assert.assertEquals(tabTitlesActual.toString(), expectedResult.toString());
+    }
 
-            String expectedResult = "MySQL";
+    @Test
+    public void testMathLanguageRow() {
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath(BROWSE_LANG)).click();
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='m.html']")).click();
 
-            getDriver().get("http://www.99-bottles-of-beer.net/");
+        WebElement mathRow = getDriver().findElement(By.xpath("//a[contains(text(),'Mathematica')]/parent::td/parent::tr"));
+        String actualResult = mathRow.getText();
+        String expectedResult = "Mathematica Brenton Bostick 03/16/06 1";
 
-            getDriver().findElement(By.xpath(
-                            "//div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")).click();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 
-            getDriver().findElement(By.xpath(
-                    "//ul[@id='submenu']/li/a[@href='m.html']")).click();
+    @Test
+    public void testNumericalLanguagesNames() {
+        int expectedResult = 10;
+        getDriver().get(BASE_URL);
+        getDriver().findElement(By.xpath(BROWSE_LANG)).click();
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='0.html']")).click();
 
-            WebElement mLet = getDriver().findElement(By.xpath("//tr[last()]/td/a"));
+        List<WebElement> numNames = getDriver().findElements(By.xpath("//tbody/tr[@onmouseover]"));
+        int actualResult = numNames.size();
 
-            String actualResult = mLet.getText();
-
-            Assert.assertEquals(actualResult, expectedResult);
-
-        }
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
