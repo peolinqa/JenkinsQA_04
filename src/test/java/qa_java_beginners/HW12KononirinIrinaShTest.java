3aaad1298634b5713eee8917cd01d8b04e7b6f26
Irina Sh <104162505+Kononirin@users.noreply.github.com>
2022-05-28 22:53:54 +0500

    Add HW12 task 3, 4, 5, 6 (#416)

--- src/test/java/qa_java_beginners/HW12KononirinIrinaShTest.java
+++ src/test/java/qa_java_beginners/HW12KononirinIrinaShTest.java
@@ -1,12 +1,11 @@
 package qa_java_beginners;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+import java.util.Arrays;
 
 public class HW12KononirinIrinaShTest extends BaseTest {
 
@@ -58,6 +57,195 @@ public class HW12KononirinIrinaShTest extends BaseTest {
                 .findElement(By.xpath("//tr[last()]/td/a"));
 
         String actualResult = lastLangMySQL.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testHeaderOfTable() {
+
+        String expectedResult = "Language Author Date Comments Rate";
+
+        getDriver().get("http://99-bottles-of-beer.net/");
+
+        WebElement menuBrowseLang = getDriver()
+                .findElement(
+                        By.xpath("//li/a[@href='/abc.html']"));
+        menuBrowseLang.click();
+
+        WebElement headerOfTable = getDriver().findElement(
+                By.xpath("//table/tbody/tr"));
+
+        String actualResult = headerOfTable.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testTableOfMathematica() {
+
+        String expectedResultOne = "Brenton Bostick";
+        String expectedResultTwo = "03/16/06";
+        String expectedResultThree = "1";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/abc.html");
+
+        WebElement subMenuM = getDriver()
+                .findElement(
+                        By.xpath("//a[@href='m.html']"));
+        subMenuM.click();
+
+        WebElement langMathematika = getDriver().findElement(
+                By.xpath("//a[@href='language-mathematica-1090."
+                        + "html']"));
+        langMathematika.click();
+
+        WebElement tableOfMathemAuthor = getDriver().findElement(
+                By.xpath("//table[@style='margin: 7px; " +
+                        "padding: 0;;']/tbody/tr[2]/td[last()]"));
+        String actualResultOne = tableOfMathemAuthor.getText();
+
+        WebElement tableOfMathemDate = getDriver().findElement(
+                By.xpath("//table[@style='margin: 7px; " +
+                        "padding: 0;;']/tbody/tr[1]/td[last()]"));
+        String actualResultTwo = tableOfMathemDate.getText();
+
+        WebElement tableOfMathemComment = getDriver().findElement(
+                By.xpath("//table[@style='margin: 7px; " +
+                        "padding: 0;;']/tbody/tr[4]/td[last()]"));
+        String actualResultThree = tableOfMathemComment.getText();
+
+        Assert.assertEquals(actualResultOne, expectedResultOne);
+        Assert.assertEquals(actualResultTwo, expectedResultTwo);
+        Assert.assertEquals(actualResultThree, expectedResultThree);
+    }
+
+    @Test
+    public void testExistTenLangsStartsDigits() {
+
+        getDriver().get("http://99-bottles-of-beer.net/");
+
+        WebElement menuBrowseLang = getDriver()
+                .findElement(
+                        By.xpath("//li/a[@href='/abc.html']")
+                );
+        menuBrowseLang.click();
+
+        WebElement submenuZeroNine = getDriver().
+                findElement(By.xpath("//ul[@id='submenu']/" +
+                        "li/a[@href='0.html']")
+                );
+        submenuZeroNine.click();
+
+        String[] countLines = new String[10];
+
+        WebElement firstLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[2]/td/a")
+                );
+        countLines[0] = firstLine.getText();
+
+        WebElement secondLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[3]/td/a")
+                );
+        countLines[1] = secondLine.getText();
+
+        WebElement thirdLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[4]/td/a")
+                );
+        countLines[2] = thirdLine.getText();
+
+        WebElement fourthLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[5]/td/a")
+                );
+        countLines[3] = fourthLine.getText();
+
+        WebElement fifthLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[6]/td/a")
+                );
+        countLines[4] = fifthLine.getText();
+
+        WebElement sixthLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[7]/td/a")
+                );
+        countLines[5] = sixthLine.getText();
+
+        WebElement seventhLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[8]/td/a")
+                );
+        countLines[6] = seventhLine.getText();
+
+        WebElement eighthLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[9]/td/a")
+                );
+        countLines[7] = eighthLine.getText();
+
+        WebElement ninethLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[10]/td/a")
+                );
+        countLines[8] = ninethLine.getText();
+
+        WebElement tenthLine = getDriver().
+                findElement(By.xpath("//table[@id='category']/" +
+                        "tbody/tr[11]/td/a")
+                );
+        countLines[9] = tenthLine.getText();
+
+        System.out.println(Arrays.toString(countLines));
+
+        Assert.assertEquals(countLines.length, 10);
+    }
+
+    @Test
+    public void testIncorrectSecurityCode() {
+
+        String expectedResult = "Error: Error: Invalid security code.";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/signv2.html");
+
+        getDriver().findElement(
+                        By.xpath("//div[@id='main']/form/p/" +
+                                "input[@name='name']"))
+                .sendKeys("Irina Sh.");
+
+        getDriver().findElement(
+                        By.xpath("//div[@id='main']/form/p/" +
+                                "input[@name='location']"))
+                .sendKeys("RF");
+
+        getDriver().findElement(
+                        By.xpath("//div[@id='main']/form/p/" +
+                                "input[@name='email']"))
+                .sendKeys("test@gmail.com");
+
+        getDriver().findElement(
+                        By.xpath("//div[@id='main']/form/p/" +
+                                "input[@name='homepage']"))
+                .sendKeys("http://test.com");
+
+        String randomDigit = "" + ((int) (Math.random() * 900) + 100);
+        getDriver().findElement(
+                        By.xpath("//div[@id='main']/form/p/" +
+                                "input[@name='captcha']"))
+                .sendKeys(randomDigit);
+
+        getDriver().findElement(
+                        By.xpath("//textarea[@name='comment']"))
+                .sendKeys("Frist comment");
+
+        getDriver().findElement(
+                By.xpath("//input[@type='submit']")).click();
+
+        String actualResult = getDriver().findElement(
+                By.xpath("//div[@id='main']/p")).getText();
 
         Assert.assertEquals(actualResult, expectedResult);
     }
