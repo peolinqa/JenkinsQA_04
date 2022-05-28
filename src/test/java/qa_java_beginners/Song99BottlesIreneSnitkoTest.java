c60e8aa7525310ad34d84f81deecd45d9203275c
IreneSnitko <103880495+IreneSnitko@users.noreply.github.com>
2022-05-28 12:04:22 +0300

    testBrowseLanguagesSubmenuMLastProgramIsLangMySQL() created (#409)

--- src/test/java/qa_java_beginners/Song99BottlesIreneSnitkoTest.java
+++ src/test/java/qa_java_beginners/Song99BottlesIreneSnitkoTest.java
@@ -1,4 +1,5 @@
 package qa_java_beginners;
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -139,7 +140,7 @@ public class Song99BottlesIreneSnitkoTest extends BaseTest {
 
     @Test
 
-    public void testBrowseLanguagesSubmenuJ () {
+    public void testBrowseLanguagesSubmenuJ() {
 
         String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
 
@@ -148,6 +149,22 @@ public class Song99BottlesIreneSnitkoTest extends BaseTest {
         getDriver().findElement(By.xpath("//li/a[@href='j.html']")).click();
 
         String actualResult = getDriver().findElement(By.xpath("//div[@id='main']/p")).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testBrowseLanguagesSubmenuMLastProgramIsLangMySQL() {
+
+        String expectedResult = "MySQL";
+
+        getDriver().get("https://www.99-bottles-of-beer.net/");
+        getDriver().findElement(By.xpath("//li/a[@href='/abc.html']")).click();
+        getDriver().findElement(By.xpath("//li/a[@href='m.html']")).click();
+
+        String actualResult = getDriver()
+                .findElement(By
+                        .xpath("//td/a[@href='language-mysql-1252.html']")).getText();
 
         Assert.assertEquals(actualResult, expectedResult);
     }
