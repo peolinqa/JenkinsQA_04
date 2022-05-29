7c3b356453aef7b830572d8153fad43a12a66c27
ServachakM <102581167+ServachakM@users.noreply.github.com>
2022-05-29 23:05:07 +0300

    Refactoring testSongLyricsText (#442)
    * testSongLyricsText refactored
    
    * testMenuContact Marolespb8HW12Test ignored

--- src/test/java/qa_java_beginners/Marolespb8HW12Test.java
+++ src/test/java/qa_java_beginners/Marolespb8HW12Test.java
@@ -3,6 +3,7 @@ package qa_java_beginners;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -24,6 +25,7 @@ public class Marolespb8HW12Test extends BaseTest {
 
     }
 
+    @Ignore
     @Test
     public void testMenuContact() {
 
