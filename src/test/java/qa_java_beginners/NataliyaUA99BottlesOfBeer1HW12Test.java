9bc7f0ed9ba998f1ab4b7d5fca4dd981b40c0922
NataliyaUA <natalivo13@gmail.com>
2022-05-29 13:34:10 +0800

    first commit  src/test/java/qa_java_beginners/NataliyaUA99BottlesOfBeer1HW12Test.java created, testBrowseLanguageMenuTest added

---
+++ src/test/java/qa_java_beginners/NataliyaUA99BottlesOfBeer1HW12Test.java
@@ -0,0 +1,38 @@
+package qa_java_beginners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class NataliyaUA99BottlesOfBeer1HW12Test extends BaseTest {
+    public static String url = "http://www.99-bottles-of-beer.net/";
+
+    @Test
+    public void testBrowseLanguageMenuTest () {
+        String expectedResult = "All languages starting with the letter J " +
+                "are shown, sorted by Language.";
+
+        getDriver().get(url);
+
+        getDriver().findElement(By.xpath("//ul[@id='menu']" +
+                "/li/a[@href='/abc.html']")).click();
+
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li" +
+                "/a[@href='j.html']")).click();
+
+        WebElement text = getDriver().findElement(By.xpath("//div[@id='main']" +
+                "/p"));
+
+        String actualResult = text.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        getDriver().quit();
+
+
+
+
+    }
+}
