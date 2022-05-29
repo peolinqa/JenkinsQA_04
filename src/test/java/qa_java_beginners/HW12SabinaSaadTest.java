c1b7c2d242e3b31fc2d24eea73fc057061573efe
sabinasaad <104028290+sabinasaad@users.noreply.github.com>
2022-05-29 15:58:25 +0300

    HW12_2and3_SabinaSaad  (#418)
    * created
    testSubMenuMLastLanguage()
    
    * created testSubMenuMLastLanguage() and testBrowseLanguagesHeaders()

--- src/test/java/qa_java_beginners/HW12SabinaSaadTest.java
+++ src/test/java/qa_java_beginners/HW12SabinaSaadTest.java
@@ -9,7 +9,7 @@ public class HW12SabinaSaadTest extends BaseTest {
 
     public static String URL = "http://www.99-bottles-of-beer.net/";
 
-    @Test(invocationCount = 20)
+    @Test
     public void testSubMenuJBrowseLanguages() {
         String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
 
@@ -18,6 +18,37 @@ public class HW12SabinaSaadTest extends BaseTest {
         getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='j.html']")).click();
         String actualResult = getDriver().findElement(By.xpath("//div/div[@id='main']/p")).getText();
 
-        Assert.assertEquals(actualResult,expectedResult);
+        Assert.assertEquals(actualResult, expectedResult);
     }
+
+    @Test
+    public void testSubMenuMLastLanguage() {
+        String expectedResult = "MySQL";
+
+        getDriver().get(URL);
+        getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']")).click();
+        getDriver().findElement(By.xpath("//ul[@id='submenu']/li/a[@href='m.html']")).click();
+
+        String actualResult = getDriver().findElement(By.xpath("//body/div/div/table/tbody/tr[last()]/td/a")).getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    public void testBrowseLanguagesHeaders() {
+        String[] expectedResult = new String[]{"Language", "Author", "Date", "Comments", "Rate"};
+        getDriver().get(URL);
+        getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']")).click();
+
+        int arrayLength = getDriver().findElements(By.xpath("//table/tbody/tr[1]/th")).size();
+        String[] actualResult = new String[arrayLength];
+        for (int i = 0; i < actualResult.length; i++) {
+            actualResult[i] = getDriver().findElement(By.xpath("//table/tbody/tr[1]/th[" + (i + 1) + "]")).getText();
+        }
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+
+
 }
