6a2b563f3c7fadbb7378907354148acf8650e6c1
SafronovaOlga <104982522+SafronovaOlga@users.noreply.github.com>
2022-05-30 14:41:48 +0200

    Refactored and cleaned SafOlgaTest (#464)
    Co-authored-by: new_name <new_email>

--- src/test/java/SafronovaOlgaTest.java
+++ src/test/java/SafronovaOlgaTest.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -29,27 +28,22 @@ public class SafronovaOlgaTest extends BaseTest {
 
     @Test
     public  void SafOlgaTest ()  {
-
-        getDriver().get("http://www.99-bottles-of-beer.net/");
-
+        final String URL = "http://www.99-bottles-of-beer.net/";
         String expectedResult = "Welcome to 99 Bottles of Beer";
 
+        getDriver().get(URL);
 
         WebElement menuBrowseLanguages = getDriver().findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']")
+                By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']")
 
         );
         menuBrowseLanguages.click();
 
-
         WebElement menuStart = getDriver().findElement(
-                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/']")
-
-        );
+                By.xpath("//ul[@id='menu']/li/a[@href='/']"));
         menuStart.click();
 
-
-        WebElement h2 =getDriver().findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/h2"));
+        WebElement h2 =getDriver().findElement(By.xpath("//div[@id='main']/h2"));
 
         String actualResult = h2.getText();
 
