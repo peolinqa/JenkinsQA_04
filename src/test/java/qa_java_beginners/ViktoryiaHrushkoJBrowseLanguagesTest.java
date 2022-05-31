fdb4315446fd5729a32e87d5c70ca8809fc4ba74
viktoryiahrushko <89564707+viktoryiahrushko@users.noreply.github.com>
2022-05-31 11:14:02 -0700

    ViktoryiaHrushkoJBrowseLanguagesTest class added, testBrowseLanguagesJ() created (#484)

---
+++ src/test/java/qa_java_beginners/ViktoryiaHrushkoJBrowseLanguagesTest.java
@@ -0,0 +1,31 @@
+package qa_java_beginners;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ViktoryiaHrushkoJBrowseLanguagesTest extends BaseTest {
+    @Test
+    public void testBrowseLanguagesJ(){
+        String expectedResult = "All languages starting with the letter J are shown, sorted by Language.";
+
+        getDriver().get("http://www.99-bottles-of-beer.net/");
+        getDriver()
+                .findElement(
+                By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='menu']/li/a[@href='/abc.html']"))
+                .click();
+
+        getDriver()
+                .findElement(
+                        By.xpath("//body/div[@id='wrap']/div[@id='navigation']/ul[@id='submenu']/li/a[@href='j.html']"))
+                .click();
+
+        WebElement jText = getDriver().findElement(By.xpath("//body/div[@id='wrap']/div[@id='main']/p"));
+        String actualResult = jText.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+    }
+}
