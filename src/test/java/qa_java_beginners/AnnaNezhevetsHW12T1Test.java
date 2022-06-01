06445af0ed00cbcde5e0c5c79c08980ac9e29d09
Anna <103545947+AnnaNezhevets@users.noreply.github.com>
2022-06-01 09:38:29 +0300

    Anna n31/05/2022 (#491)
    * Task TC_12_02 added
    
    * Added task TC_12_03

--- src/test/java/qa_java_beginners/AnnaNezhevetsHW12T1Test.java
+++ src/test/java/qa_java_beginners/AnnaNezhevetsHW12T1Test.java
@@ -21,4 +21,43 @@ public class AnnaNezhevetsHW12T1Test extends BaseTest{
 
         Assert.assertEquals(subMenuJAll.getText(), expectedResult);
     }
+
+    @Test
+    public void testSubmenuMMySQL(){
+        String expectedResult = "MySQL";
+
+        getDriver().get(URL);
+        WebElement browseLanguage = getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']"));
+        browseLanguage.click();
+        WebElement subMenuM = getDriver().findElement(By.xpath("//ul[@id='submenu']//li/a[@href='m.html']"));
+        subMenuM.click();
+        WebElement lastElementMySQL = getDriver().findElement(
+                By.xpath("//div[@id='main']/table[@id='category']//tr/td/a[@href='language-mysql-1252.html']"));
+
+        Assert.assertEquals(lastElementMySQL.getText(), expectedResult);
+    }
+
+    @Test
+    public void testTableNames(){
+        String[] expectedResult = new String[]{"Language", "Author", "Date", "Comments", "Rate"};
+
+        getDriver().get(URL);
+        WebElement browseLanguage = getDriver().findElement(By.xpath("//ul[@id='menu']/li/a[@href='/abc.html']"));
+        browseLanguage.click();
+        WebElement language = getDriver().findElement(
+                By.xpath("//div[@id='main']/table[@id='category']/tbody/tr//th[@style='width: 40%;']"));
+        WebElement author = getDriver().findElement(
+                By.xpath("//div[@id='main']/table[@id='category']/tbody/tr//th[@style='width: 30%;']"));
+        WebElement date = getDriver().findElement(
+                By.xpath("//div[@id='main']/table[@id='category']/tbody/tr//th[(contains(text(),'Date'))]"));
+        WebElement comments = getDriver().findElement(
+                By.xpath("//div[@id='main']/table[@id='category']/tbody/tr//th[(contains(text(),'Comments'))]"));
+        WebElement rate = getDriver().findElement(
+                By.xpath("//div[@id='main']/table[@id='category']/tbody/tr//th[(contains(text(),'Rate'))]"));
+
+        String[] actualResult = new String[]{language.getText(), author.getText(), date.getText(), comments.getText(),
+        rate.getText()};
+
+        Assert.assertEquals(actualResult, expectedResult);
+    }
 }
