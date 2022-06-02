1b9f154fcb8225e1e79b23519afd42a002ba227d
xbrookx <72659405+xbrookx@users.noreply.github.com>
2022-06-02 23:31:57 +0300

    added test ManageCredentials to class ManageCredentialsTest TC_121.001 (#523)
    Steps:
    1 log in to "Jenkins"
    2 click to menu "Manage "Jenkins"
    3 click to "Manage Credentials"
    4 check current icon size
    5 click another size
    6 check current icon size
    
    Expected results:
    Icon size in step 4 not eguals icon size in step 6

---
+++ src/test/java/ManageCredentialsTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+public class ManageCredentialsTest extends BaseTest {
+    private static final String ICON_XPATH = "//td[@data='Jenkins Credentials Provider']";
+
+    @Test
+    public void testManageCredentials() {
+        getDriver().findElement(By.linkText("Manage Jenkins")).click();
+        getDriver().findElement(By.xpath("//dt[contains(text(),'Manage Credentials')]")).click();
+
+        String iconButtonName = getDriver().findElement(
+                By.xpath("//li[@class='jenkins-icon-size__items-item']")).getText().substring(0, 1);
+
+        String iconSizeBefore = getDriver().findElement(
+                By.xpath(ICON_XPATH)).getCssValue("height");
+
+        if (iconButtonName.equals("L")) {
+            WebElement iconButtonS = getDriver().findElement(By.xpath("//a[@href='/iconSize?16x16']"));
+            iconButtonS.click();
+        } else {
+            WebElement iconButtonL = getDriver().findElement(By.xpath("//a[@href='/iconSize?32x32']"));
+            iconButtonL.click();
+        }
+        String iconSizeAfter = getDriver().findElement(
+                By.xpath(ICON_XPATH)).getCssValue("height");
+
+        Assert.assertNotEquals(iconSizeAfter, iconSizeBefore);
+    }
+}
