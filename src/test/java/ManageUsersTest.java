930694d20b543bcab2c2ea016ca0abe18dd8af57
Viktor Podgornov <77942592+ViktorPodgornov@users.noreply.github.com>
2022-06-02 02:22:34 -0400

    Added test <testUserCanDeleteUser> (#516)

--- src/test/java/ManageUsersTest.java
+++ src/test/java/ManageUsersTest.java
@@ -8,18 +8,18 @@ import java.util.List;
 
 public class ManageUsersTest extends BaseTest {
 
+    private static final String USER_NAME = "viktorp";
+    private static final String FULL_NAME = "Viktor P";
+
     @Test
     public void testUserCanCreateNewUser() {
 
         getDriver().get("http://localhost:8080/securityRealm/addUser");
 
-        String username = "viktorp";
-        String fullname = "Viktor P";
-
-        getDriver().findElement(By.id("username")).sendKeys(username);
+        getDriver().findElement(By.id("username")).sendKeys(USER_NAME);
         getDriver().findElement(By.name("password1")).sendKeys("123456ABC");
         getDriver().findElement(By.name("password2")).sendKeys("123456ABC");
-        getDriver().findElement(By.name("fullname")).sendKeys(fullname);
+        getDriver().findElement(By.name("fullname")).sendKeys(FULL_NAME);
         getDriver().findElement(By.name("email")).sendKeys("testemail.@gmail.com");
 
         getDriver().findElement(By.id("yui-gen1-button")).click();
@@ -27,10 +27,26 @@ public class ManageUsersTest extends BaseTest {
         List<WebElement> users = getDriver().findElements(By.xpath("//table[@id='people']/tbody/tr"));
 
         for (WebElement user : users) {
-            if (user.getText().contains(username) && user.getText().contains(fullname)) {
+            if (user.getText().contains(USER_NAME) && user.getText().contains(FULL_NAME)) {
 
                 Assert.assertTrue(user.isDisplayed());
             }
+        }
+    }
+
+    @Test(dependsOnMethods = {"testUserCanCreateNewUser"})
+    public void testUserCanDeleteUser() {
+
+        getDriver().get("http://localhost:8080/securityRealm/");
+
+        getDriver().findElement(By.xpath(String.format("//a[contains(@href, '%s/delete')]", USER_NAME))).click();
+        getDriver().findElement(By.xpath("//button[@id='yui-gen1-button']")).click();
+
+        List<WebElement> users = getDriver().findElements(By.xpath("//table[@id='people']/tbody/tr"));
+
+        for (WebElement user : users) {
+
+            Assert.assertFalse(user.getText().contains(USER_NAME) && user.getText().contains(FULL_NAME));
         }
     }
 }
