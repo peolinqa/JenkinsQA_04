ee93d357f0472843123a79f96da727e1964adc3f
foxtiptop <imonthismail@gmail.com>
2022-06-02 22:37:40 -0700

    freestyle project rename test added

--- src/test/java/RusMJenkinsTest.java
+++ src/test/java/RusMJenkinsTest.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -18,5 +20,31 @@ public class RusMJenkinsTest extends BaseTest {
 
         assertTrue(getDriver().findElement(By.xpath("//tr[@id='job_item 123']/td[3]/a"))
                 .isDisplayed());
+    }
+
+    @Test
+    public void testFreestyleRename() {
+        Actions dropdown = new Actions(getDriver());
+        dropdown.moveToElement(getDriver().findElement(By
+                .xpath("//tr[@id='job_item 123']/td[3]/a"))).perform();
+
+        getDriver().findElement(By.xpath("//div[@id='menuSelector']")).click();
+        dropdown.moveToElement(getDriver().findElement(By
+                        .xpath("/html/body/div[2]/div/div[2]/div[2]/div/div/div/div[1]/ul/li[6]/a")))
+                            .click().perform();
+
+        getDriver().findElement(By.xpath("//div[@id='main-panel']/form/div[1]/div[1]/div[2]/input"))
+                .clear();
+        getDriver().findElement(By.xpath("//div[@id='main-panel']/form/div[1]/div[1]/div[2]/input"))
+                .sendKeys("project987");
+        getDriver().findElement(By.xpath("//button[@id='yui-gen1-button']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id=\"main-panel\"]/h1"))
+                .getText(), "Project project987");
+
+        getDriver().findElement(By.xpath("//div/span/a/span[2]")).click();
+
+        Assert.assertEquals(getDriver().findElement(By
+                .xpath("//tr[@id='job_project987']/td[3]/a")).getText(), "project987");
     }
 }
