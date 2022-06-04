1d4187bc20dd295581e5474f97c4f79b0830130d
Olga F <105674075+OVFgit@users.noreply.github.com>
2022-06-04 17:28:20 +0300

    changes to class ChangeFolderNameOVFTest for RF_010.001 (#582)
    * changes to class ChangeFolderNameOVFTest for RF_010.001
    
    * changes to class ChangeFolderNameOVFTest for RF_010.001 updated

--- src/test/java/ChangeFolderNameOVFTest.java
+++ src/test/java/ChangeFolderNameOVFTest.java
@@ -8,6 +8,16 @@ import java.util.concurrent.TimeUnit;
 
 public class ChangeFolderNameOVFTest extends BaseTest {
 
+    public String createRandomFolderName() {
+        String folderNameSubstrate = "abcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder builder = new StringBuilder("");
+        for (int i = 0; i < 10; i++) {
+            builder.append(folderNameSubstrate.charAt((int)(Math.random() * folderNameSubstrate.length())));
+        }
+        String folderName = builder.toString();
+        return folderName;
+    }
+
     public void сreateFolder(String folderName) {
         getDriver().findElement(By.xpath("//span[normalize-space(text())='New Item']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(folderName);
@@ -15,10 +25,27 @@ public class ChangeFolderNameOVFTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
     }
 
+    public void deleteCreatedFolder(String newFolderName) {
+        getDriver().manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        getDriver().findElement(By.xpath("//a[normalize-space(text())='Dashboard']")).click();
+
+        Actions action = new Actions(getDriver());
+        action.moveToElement(getDriver().findElement(By.xpath("//a[@href='job/" + newFolderName + "/']")))
+                .build().perform();
+
+        action.moveToElement(getDriver().findElement(By.xpath(
+                        "//div[@id='menuSelector']")))
+                .click().build().perform();
+        action.moveToElement(getDriver().findElement(By.xpath(
+                        "//a[@href='/job/" + newFolderName + "/delete']")))
+                .click().build().perform();
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
     @Test
     public void testRenameFolderPositive() {
-        String folderName = "OVFtestfolder";
-        String newFolderName = "OVFtestfolder-2";
+        String folderName = createRandomFolderName();
+        String newFolderName = createRandomFolderName();
 
         сreateFolder(folderName);
         getDriver().manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
@@ -41,6 +68,7 @@ public class ChangeFolderNameOVFTest extends BaseTest {
 
         getDriver().manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
         String actualResult = getDriver().findElement(By.xpath("//h1")).getText();
+        deleteCreatedFolder(newFolderName);
 
         Assert.assertEquals(newFolderName, actualResult);
     }
