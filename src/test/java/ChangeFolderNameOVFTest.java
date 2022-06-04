af3ff2e533d062e5987a1237a0d7f939da82e289
Olga F <105674075+OVFgit@users.noreply.github.com>
2022-06-05 00:43:09 +0300

    changes to class ChangeFolderNameOVFTest for TC_010.002 (#594)
    * changes to class ChangeFolderNameOVFTest for TC_010.002
    
    * updated changes to class ChangeFolderNameOVFTest for TC_010.002

--- src/test/java/ChangeFolderNameOVFTest.java
+++ src/test/java/ChangeFolderNameOVFTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ChangeFolderNameOVFTest extends BaseTest {
 
-    public String createRandomFolderName() {
+    private String createRandomName() {
         String folderNameSubstrate = "abcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder builder = new StringBuilder("");
         for (int i = 0; i < 10; i++) {
@@ -18,14 +18,14 @@ public class ChangeFolderNameOVFTest extends BaseTest {
         return folderName;
     }
 
-    public void сreateFolder(String folderName) {
+    private void сreateFolder(String folderName) {
         getDriver().findElement(By.xpath("//span[normalize-space(text())='New Item']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(folderName);
         getDriver().findElement(By.xpath("//li[contains(@class,'folder_Folder')]")).click();
         getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
     }
 
-    public void deleteCreatedFolder(String newFolderName) {
+    private void deleteCreatedFolder(String newFolderName) {
         getDriver().manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
         getDriver().findElement(By.xpath("//a[normalize-space(text())='Dashboard']")).click();
 
@@ -42,10 +42,16 @@ public class ChangeFolderNameOVFTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[@type='submit']")).click();
     }
 
+    private void cleareSendClick(String newFolderName) {
+        getDriver().findElement(By.xpath("//input[@checkdependson='newName']")).clear();
+        getDriver().findElement(By.xpath("//input[@checkdependson='newName']")).sendKeys(newFolderName);
+        getDriver().findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
     @Test
     public void testRenameFolderPositive() {
-        String folderName = createRandomFolderName();
-        String newFolderName = createRandomFolderName();
+        String folderName = createRandomName();
+        String newFolderName = createRandomName();
 
         сreateFolder(folderName);
         getDriver().manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
@@ -71,5 +77,50 @@ public class ChangeFolderNameOVFTest extends BaseTest {
         deleteCreatedFolder(newFolderName);
 
         Assert.assertEquals(newFolderName, actualResult);
+    }
+
+    @Test
+    public void testRenameFolderWithUnsafeCharacters() {
+        String folderName = createRandomName();
+        String newFolderName = "";
+        String unsafeCharacters = "!@#$%^*/|\\;:?";
+
+        getDriver().manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+        сreateFolder(folderName);
+        getDriver().findElement(By.xpath("//a[normalize-space(text())='Dashboard']")).click();
+
+        Actions action = new Actions(getDriver());
+        action.moveToElement(getDriver().findElement(By.xpath("//a[@href='job/" + folderName + "/']")))
+                .build().perform();
+        action.moveToElement(getDriver().findElement(By.xpath("//div[@id='menuSelector']")))
+                .click().build().perform();
+        action.moveToElement(getDriver().findElement(By.xpath(
+                        "//a[@href='/job/" + folderName + "/confirm-rename']")))
+                .click().build().perform();
+
+        for (int i = 0; i < unsafeCharacters.length(); i++) {
+            newFolderName = unsafeCharacters.substring(i, (i + 1));
+            cleareSendClick(newFolderName);
+            String expectedResult = "‘" + newFolderName + "’ is an unsafe character";
+            Assert.assertEquals(expectedResult,
+                    getDriver().findElement(By.xpath("//div[@id='main-panel']//p")).getText());
+            getDriver().navigate().back();
+        }
+
+        newFolderName = "&";
+        cleareSendClick(newFolderName);
+        String expectedResult = "‘&amp;’ is an unsafe character";
+        Assert.assertEquals(expectedResult,
+                getDriver().findElement(By.xpath("//div[@id='main-panel']//p")).getText());
+        getDriver().navigate().back();
+
+        newFolderName = ".";
+        cleareSendClick(newFolderName);
+        expectedResult = "“.” is not an allowed name";
+        Assert.assertEquals(expectedResult,
+                getDriver().findElement(By.xpath("//div[@id='main-panel']//p")).getText());
+        getDriver().navigate().back();
+
+        deleteCreatedFolder(folderName);
     }
 }
