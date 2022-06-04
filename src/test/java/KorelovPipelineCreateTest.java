b0e7dc7a2227925702db360a9526e89945ad988a
Maksim <korelov@gmail.com>
2022-06-04 22:39:33 +0300

    AT_017.010 , AT_017.005   Pipeline (#586)
     add AT_017.010 , AT_017.005

--- src/test/java/KorelovPipelineCreateTest.java
+++ src/test/java/KorelovPipelineCreateTest.java
@@ -6,17 +6,21 @@ import runner.BaseTest;
 
 import java.util.List;
 
-
 public class KorelovPipelineCreateTest extends BaseTest {
-    @Test
-    public void testCreatePipeline() {
 
-        final String namePipeline = "NewPipeline";
-
+    private void createPipeline(String namePipeline) {
         getDriver().findElement(By.xpath("//a[@title='New Item']")).click();
         getDriver().findElement(By.id("name")).sendKeys(namePipeline);
         getDriver().findElement(By.xpath("//li[contains(@class,'WorkflowJob')]")).click();
         getDriver().findElement(By.id("ok-button")).click();
+    }
+
+    @Test
+    public void testTC_017_006CreatePipeline() {
+
+        final String namePipeline = "NewPipeline";
+
+        createPipeline(namePipeline);
         getDriver().findElement(By.id("yui-gen6-button")).click();
 
         Assert.assertTrue(getDriver().findElement(By.xpath("//h1"))
@@ -33,5 +37,38 @@ public class KorelovPipelineCreateTest extends BaseTest {
                 break;
             }
         }
+    }
+
+    @Test
+    public void testTC_017_005HelpTooltipsText() {
+
+        final String namePipeline = "NewPipelineTooltips";
+
+        createPipeline(namePipeline);
+
+        List<WebElement> listOfChecBoxWithHelps = getDriver()
+                .findElements(By.xpath("//div[contains(@hashelp, 'true')]//label"));
+
+        List<WebElement> checkBoxHelpsText = getDriver()
+                .findElements(By.xpath("//div[contains(@hashelp, 'true')]//a"));
+
+        for (int i = 0; i < listOfChecBoxWithHelps.size(); i++) {
+            Assert.assertEquals(checkBoxHelpsText.get(i).getAttribute("title")
+                    .replace("Help for feature: ", ""), listOfChecBoxWithHelps.get(i).getText());
+        }
+    }
+
+    @Test
+    public void testTC_017_010ApplyNotificationAlert(){
+
+        final String namePipeline = "NewPipelineNotification";
+
+        createPipeline(namePipeline);
+        getDriver().findElement(By.id("yui-gen5-button")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.id("notification-bar"))
+                .getText(),"Saved");
+
+        getDriver().findElement(By.id("yui-gen6-button")).click();
     }
 }
