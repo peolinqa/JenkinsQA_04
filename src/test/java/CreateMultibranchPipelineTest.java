5047f3423ed4d762481f8d66b375a04ea65b1d54
azellus-tercia <azellustercia@gmail.com>
2022-06-04 21:59:06 +0500

    Shorten the name of a project.

--- src/test/java/CreateMultibranchPipelineTest.java
+++ src/test/java/CreateMultibranchPipelineTest.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class CreateMultibranchPipelineTest extends BaseTest {
 
-    private final String PROJECT_NAME = "MultibranchPipelineTestProject";
+    private final String PROJECT_NAME = "MBPLTestProject";
     private final char[] unsafeCharacters = {'!', '@', '#', '$', '%', '^', '&', '*', '[', ']', ';', ':'};
 
     private void deleteProject() {
