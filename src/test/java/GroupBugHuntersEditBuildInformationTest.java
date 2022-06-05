132dc3ba8b77d17b22f177e216684952ebcd585a
imperiya <darimper@gmail.com>
2022-06-05 19:28:21 -0400

    Add TC_032_002, TC_032_003

--- src/test/java/GroupBugHuntersEditBuildInformationTest.java
+++ src/test/java/GroupBugHuntersEditBuildInformationTest.java
@@ -29,8 +29,34 @@ public class GroupBugHuntersEditBuildInformationTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[@type='submit']")).submit();
         String buildName = getDriver().findElement(By.xpath("//span[@class='jenkins-icon-adjacent']")).getText();
         String buildDescription = getDriver().findElement(By.id("description")).getText();
-        Assert.assertEquals(getDriver().getCurrentUrl(),"http://localhost:8080/job/NewFreestyleProject/lastBuild/");
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/job/NewFreestyleProject/lastBuild/");
         Assert.assertTrue(buildName.contains("Build New build 123"));
         Assert.assertTrue(buildDescription.contains("Build 123 description test"));
+    }
+
+    @Test(dependsOnMethods = {"testVerifyChangeOnBuildStatusPage_TC_032_001"})
+    public void testVerifyChangeOnProjectStatusPage_TC_032_002() {
+        getDriver().findElement(By.xpath("//a[@href='job/NewFreestyleProject/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='lastBuild/']")).click();
+        getDriver().findElement(By.xpath("//span[text()='Back to Project']")).click();
+        String buildName = getDriver().findElement(By.xpath("//a[text()='New build 123']")).getText();
+        String buildDescription = getDriver().findElement(By.xpath("//div[@class='pane desc indent-multiline']")).getText();
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/job/NewFreestyleProject/");
+        Assert.assertTrue(buildName.contains("New build 123"));
+        Assert.assertTrue(buildDescription.contains("Build 123 description test"));
+
+    }
+
+    @Test(dependsOnMethods = {"testVerifyChangeOnBuildStatusPage_TC_032_001"})
+    public void testVerifyChangeOnBuildHistoryPage_TC_032_003() {
+        getDriver().findElement(By.xpath("//a[@href='job/NewFreestyleProject/']")).click();
+        getDriver().findElement(By.xpath("//a[@href='lastBuild/']")).click();
+        getDriver().findElement(By.xpath("//span[text()='Back to Project']")).click();
+        getDriver().findElement(By.xpath("//span[text()='Back to Dashboard']")).click();
+        getDriver().findElement(By.xpath("//span[text()='Build History']")).click();
+        String buildName = getDriver().findElement(By.xpath("//a[@href='/job/NewFreestyleProject/1/']")).getText();
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/view/all/builds");
+        Assert.assertTrue(buildName.contains("New build 123"));
+
     }
 }
