f9cae7f7df89f23e4ec1d1b6723edd1b336b7521
Maksim <korelov@gmail.com>
2022-06-05 18:38:52 +0300

    AT_021.001 delete pipeline (#591)

--- src/test/java/KorelovPipelineTest.java
+++ src/test/java/KorelovPipelineTest.java
@@ -6,7 +6,7 @@ import runner.BaseTest;
 
 import java.util.List;
 
-public class KorelovPipelineCreateTest extends BaseTest {
+public class KorelovPipelineTest extends BaseTest {
 
     private void createPipeline(String namePipeline) {
         getDriver().findElement(By.xpath("//a[@title='New Item']")).click();
@@ -15,21 +15,29 @@ public class KorelovPipelineCreateTest extends BaseTest {
         getDriver().findElement(By.id("ok-button")).click();
     }
 
+    private List<WebElement> getActualDashboardProject() {
+        return getDriver().findElements
+                (By.xpath("//a[@class='jenkins-table__link model-link inside']"));
+    }
+
+    private void saveButtonPipelineClick() {
+        getDriver().findElement(By.id("yui-gen6-button")).click();
+    }
+
     @Test
     public void testTC_017_006CreatePipeline() {
 
         final String namePipeline = "NewPipeline";
 
         createPipeline(namePipeline);
-        getDriver().findElement(By.id("yui-gen6-button")).click();
+        saveButtonPipelineClick();
 
         Assert.assertTrue(getDriver().findElement(By.xpath("//h1"))
                 .getText().contains(namePipeline));
 
         getDriver().findElement(By.xpath("//a[@title='Back to Dashboard']")).click();
 
-        List<WebElement> actualDashboardProject = getDriver()
-                .findElements(By.xpath("//a[@class='jenkins-table__link model-link inside']"));
+        List<WebElement> actualDashboardProject = getActualDashboardProject();
 
         for (WebElement webElement : actualDashboardProject) {
             if (webElement.getText().contains(namePipeline)) {
@@ -46,20 +54,20 @@ public class KorelovPipelineCreateTest extends BaseTest {
 
         createPipeline(namePipeline);
 
-        List<WebElement> listOfChecBoxWithHelps = getDriver()
+        List<WebElement> listOfCheckBoxWithHelps = getDriver()
                 .findElements(By.xpath("//div[contains(@hashelp, 'true')]//label"));
 
         List<WebElement> checkBoxHelpsText = getDriver()
                 .findElements(By.xpath("//div[contains(@hashelp, 'true')]//a"));
 
-        for (int i = 0; i < listOfChecBoxWithHelps.size(); i++) {
+        for (int i = 0; i < listOfCheckBoxWithHelps.size(); i++) {
             Assert.assertEquals(checkBoxHelpsText.get(i).getAttribute("title")
-                    .replace("Help for feature: ", ""), listOfChecBoxWithHelps.get(i).getText());
+                    .replace("Help for feature: ", ""), listOfCheckBoxWithHelps.get(i).getText());
         }
     }
 
     @Test
-    public void testTC_017_010ApplyNotificationAlert(){
+    public void testTC_017_010ApplyNotificationAlert() {
 
         final String namePipeline = "NewPipelineNotification";
 
@@ -67,8 +75,37 @@ public class KorelovPipelineCreateTest extends BaseTest {
         getDriver().findElement(By.id("yui-gen5-button")).click();
 
         Assert.assertEquals(getDriver().findElement(By.id("notification-bar"))
-                .getText(),"Saved");
+                .getText(), "Saved");
 
-        getDriver().findElement(By.id("yui-gen6-button")).click();
+        saveButtonPipelineClick();
+    }
+
+    @Test
+    public void testTC_021_001DeletePipelineFromSideMenu() {
+
+        final String namePipeline = "DeletePipelineFromSideMenu";
+
+        createPipeline(namePipeline);
+        saveButtonPipelineClick();
+
+        getDriver().findElement(By.xpath("//a[@class='task-link  confirmation-link']")).click();
+
+        getDriver().switchTo().alert().accept();
+
+        try {
+            if ((getDriver().findElement(By.xpath("//h1")).getText().contains("Welcome to Jenkins!"))) {
+                Assert.assertTrue(true);
+            }
+        } catch (Exception e) {
+            List<WebElement> actualDashboardProject = getActualDashboardProject();
+            for (WebElement webElement : actualDashboardProject) {
+                if (webElement.getText().contains(namePipeline)) {
+                    Assert.fail();
+                    break;
+                } else {
+                    Assert.assertTrue(true);
+                }
+            }
+        }
     }
 }
