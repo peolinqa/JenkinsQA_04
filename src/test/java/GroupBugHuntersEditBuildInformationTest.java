d62981f1176c38e43bbcc331486417203b1f1b81
imperiya <darimper@gmail.com>
2022-06-05 19:50:45 -0400

    Add TC_032_002, TC_032_003 add spaces

--- src/test/java/GroupBugHuntersEditBuildInformationTest.java
+++ src/test/java/GroupBugHuntersEditBuildInformationTest.java
@@ -27,8 +27,10 @@ public class GroupBugHuntersEditBuildInformationTest extends BaseTest {
         getDriver().findElement(By.xpath("//input[@name='displayName']")).sendKeys("New build 123");
         getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys("Build 123 description test");
         getDriver().findElement(By.xpath("//button[@type='submit']")).submit();
+
         String buildName = getDriver().findElement(By.xpath("//span[@class='jenkins-icon-adjacent']")).getText();
         String buildDescription = getDriver().findElement(By.id("description")).getText();
+
         Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/job/NewFreestyleProject/lastBuild/");
         Assert.assertTrue(buildName.contains("Build New build 123"));
         Assert.assertTrue(buildDescription.contains("Build 123 description test"));
@@ -39,8 +41,10 @@ public class GroupBugHuntersEditBuildInformationTest extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='job/NewFreestyleProject/']")).click();
         getDriver().findElement(By.xpath("//a[@href='lastBuild/']")).click();
         getDriver().findElement(By.xpath("//span[text()='Back to Project']")).click();
+
         String buildName = getDriver().findElement(By.xpath("//a[text()='New build 123']")).getText();
         String buildDescription = getDriver().findElement(By.xpath("//div[@class='pane desc indent-multiline']")).getText();
+
         Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/job/NewFreestyleProject/");
         Assert.assertTrue(buildName.contains("New build 123"));
         Assert.assertTrue(buildDescription.contains("Build 123 description test"));
@@ -54,7 +58,9 @@ public class GroupBugHuntersEditBuildInformationTest extends BaseTest {
         getDriver().findElement(By.xpath("//span[text()='Back to Project']")).click();
         getDriver().findElement(By.xpath("//span[text()='Back to Dashboard']")).click();
         getDriver().findElement(By.xpath("//span[text()='Build History']")).click();
+
         String buildName = getDriver().findElement(By.xpath("//a[@href='/job/NewFreestyleProject/1/']")).getText();
+
         Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/view/all/builds");
         Assert.assertTrue(buildName.contains("New build 123"));
 
