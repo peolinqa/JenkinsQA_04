9d04e3f084accc537c1bc46c705929b575a4e3b1
Ruslan <47644886+RuslanG13@users.noreply.github.com>
2022-06-06 00:45:18 +0300

    Refactoring previously tests and add testPipelineGroovyPageOpening  method test (#598)
    * Refactoring previously test and add testPipelineGroovyPageOpening test

--- src/test/java/RuslanGPipelineCreatePipelineTest.java
+++ src/test/java/RuslanGPipelineCreatePipelineTest.java
@@ -1,8 +1,6 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
@@ -10,30 +8,22 @@ import java.util.Date;
 import java.util.List;
 
 public class RuslanGPipelineCreatePipelineTest extends BaseTest {
-    private JavascriptExecutor javascriptExecutor;
-    private Date date;
-
-    @BeforeMethod
-    public void beforeTest() {
-        javascriptExecutor = (JavascriptExecutor) getDriver();
-        date = new Date();
-    }
-
-    @Test(description = "TC_017.008")
-    public void testPipelineSyntaxPageOpening() {
+    private void createPipelineProjectPipelineTab() {
+        Date date = new Date();
 
         getDriver().findElement(By.xpath("//a[@title='New Item']")).click();
         getDriver().findElement(By.xpath("//input[@id='name']"))
                 .sendKeys("Ruslan Gudenko Pipeline Project+" + date.getTime());
         getDriver().findElement(By.xpath("//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']"))
                 .click();
-
-        WebElement okButton = getDriver().findElement(By.xpath("//button[@id='ok-button']"));
-        javascriptExecutor.executeScript("arguments[0].scrollIntoView();", okButton);
-        okButton.click();
-
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
         getDriver().findElement(By.xpath("//div[@class='tab config-section-activator config_pipeline']"))
                 .click();
+    }
+
+    @Test(description = "TC_017.008")
+    public void testPipelineSyntaxPageOpening() {
+        createPipelineProjectPipelineTab();
 
         String pipelineSyntaxLink = getDriver().findElement(By.xpath("//a[@href='pipeline-syntax']"))
                 .getAttribute("href");
@@ -43,5 +33,15 @@ public class RuslanGPipelineCreatePipelineTest extends BaseTest {
         String breadcrumbsPipelineSyntaxPageItem = breadcrumbsOfLinksMenu.get(2).getAttribute("href");
 
         Assert.assertTrue(breadcrumbsPipelineSyntaxPageItem.contains("pipeline-syntax"));
+    }
+
+    @Test(description = "TC_017.012")
+    public void testPipelineGroovyPageOpening() {
+        createPipelineProjectPipelineTab();
+
+        String useGroovySandboxCheckbox = getDriver().findElement(By.xpath(
+                "//input[@name='_.sandbox']")).getAttribute("checked");
+
+        Assert.assertTrue(useGroovySandboxCheckbox.contains("true"));
     }
 }
