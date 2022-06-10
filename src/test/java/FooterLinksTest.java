5bd00c3c38ce9c62781cbdfc7a8264277fb5f728
Lada <90631144+LedaLada@users.noreply.github.com>
2022-06-10 10:00:27 +0300

    Tests for Footer Links (#727)
    AT_132.002| Footer > Check links

---
+++ src/test/java/FooterLinksTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.Set;
+
+public class FooterLinksTest extends BaseTest {
+
+    @Test
+    public void linkRestApiTest() {
+        getDriver().findElement(By.xpath("//div[@class = 'page-footer__links rest_api hidden-xs']")).click();
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/api/");
+    }
+
+    @Test
+    public void linkJenkinsVersionTest() {
+        getDriver().findElement(By.xpath("//a[contains(text(),'Jenkins')]")).click();
+
+        Set<String> handles = getDriver().getWindowHandles();
+        String current = getDriver().getWindowHandle();
+        handles.remove(current);
+        String newTab = handles.iterator().next();
+        getDriver().switchTo().window(newTab);
+
+        Assert.assertEquals(getDriver().getCurrentUrl(),"https://www.jenkins.io/");
+        getDriver().switchTo().window(current);
+
+    }
+
+}
