3df85843e25b602f56ebd5ddcd176f6aa1c1087d
ksutitova <103765645+ksutitova@users.noreply.github.com>
2022-06-10 09:23:17 +0300

    Tc 042 002 add description test (#717)
    * added PROJECT_NAM in xpath
    
    * TC_042_002_AddDescriptionTest

--- src/test/java/CreateMultiConfigurationProject_US_041_Test.java
+++ src/test/java/CreateMultiConfigurationProject_US_041_Test.java
@@ -20,17 +20,30 @@ public class CreateMultiConfigurationProject_US_041_Test extends BaseTest {
         getDriver().switchTo().alert().accept();
     }
 
-
     @Test
     public void TC_041_006_CreateMultiConfigurationProjectTest() {
         String expectedResult = "Mcproject";
 
         createMCProject(PROJECT_NAME);
 
-        String actualResult = getDriver().findElement(By.xpath("//div/ul/li/a[@href='/job/Mcproject/']")).getText();
+        String actualResult = getDriver().findElement(By.xpath("//div/ul/li/a[@href='/job/" + PROJECT_NAME +"/']")).getText();
         Assert.assertEquals(actualResult, expectedResult);
 
         deleteMCProject();
     }
 
+    @Test
+    public void TC_042_002_AddDescriptionTest(){
+        String expectedResult = "test";
+        createMCProject(PROJECT_NAME);
+
+        getDriver().findElement(By.id("description-link")).click();
+        getDriver().findElement(By.xpath("//div/textarea[@name=\"description\"]")).sendKeys("test");
+        getDriver().findElement(By.id("yui-gen2-button")).click();
+
+        String actualResult = getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText();
+        Assert.assertEquals(actualResult, expectedResult);
+
+        deleteMCProject();
+    }
 }
