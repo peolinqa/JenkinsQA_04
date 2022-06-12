60a788a4d25357616b65ad7d68c33b7abdd847f2
sashokdzr <43587497+sashokdzr@users.noreply.github.com>
2022-06-12 20:54:39 +0300

    Add TC_001_037_CreateNewFreestyleProject_AD (#741)

--- src/test/java/DreamTeamJavaGroupTest.java
+++ src/test/java/DreamTeamJavaGroupTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
@@ -8,6 +9,9 @@ public class DreamTeamJavaGroupTest extends BaseTest {
 
     public static final String FOOTER_REST_API = "//div[@class='page-footer__links rest_api hidden-xs']";
     public static final String REST_API_PAGE_HEADER = "//div[@id='main-panel']/h1";
+    public static final String ITEMNAME = "Valid item name" + (int) (Math.random() * 1000);
+    public static final String CREATE_PROJECT_SAVE_BUTTON = "//div[@class='bottom-sticker-inner']//button[@type='submit']";
+    public static final String DASHBOARD_BUTTON = "//a[text()='Dashboard']";
 
     @Ignore
     @Test(enabled = false)
@@ -15,5 +19,19 @@ public class DreamTeamJavaGroupTest extends BaseTest {
 
         getDriver().findElement(By.xpath(FOOTER_REST_API)).click();
         Assert.assertEquals(getDriver().findElement(By.xpath(REST_API_PAGE_HEADER)).getText(), "REST API");
+    }
+
+    @Test
+    public void test_TC_001_037_CreateNewFreestyleProject_AD() {
+        getDriver().findElement(By.className("task-link-text")).click();
+        getDriver().findElement(By.id("name")).sendKeys(ITEMNAME);
+        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getWait20().until(ExpectedConditions.elementToBeClickable(By.xpath(CREATE_PROJECT_SAVE_BUTTON)));
+        getDriver().findElement(By.xpath(CREATE_PROJECT_SAVE_BUTTON)).click();
+        getWait20().until(ExpectedConditions.elementToBeClickable(By.xpath(DASHBOARD_BUTTON)));
+        getDriver().findElement(By.xpath(DASHBOARD_BUTTON)).click();
+        String temp = getDriver().findElement(By.id("job_" + ITEMNAME)).getText();
+        Assert.assertTrue(temp.contains(ITEMNAME));
     }
 }
