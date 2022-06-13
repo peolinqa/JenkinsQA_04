6eac16ce29646c6a9e583130bb88924217e5587c
umaybeen <umaybeen@gmail.com>
2022-06-12 22:18:20 -0700

    clean up

--- src/test/java/CreateFreestyleProjectTest.java
+++ src/test/java/CreateFreestyleProjectTest.java
@@ -1,26 +1,26 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 public class CreateFreestyleProjectTest extends BaseTest {
+
     @Test
     public void testCreateFreestyleProjectValidName() {
 
-        String expectedResult = "MSTest";
+        String textString = "MSTest";
 
         getDriver().findElement(By.className("task-link-text")).click();
-        getDriver().findElement(By.id("name")).sendKeys("MSTest");
+        getDriver().findElement(By.id("name")).sendKeys(textString);
 
         getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.xpath("//div[@id='bottom-sticker']//button[@type='submit']")).click();
         getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']//a[@href='/']")).click();
+
         String actualResult = getDriver().findElement(By.id("main-panel")).findElement(By.linkText("MSTest")).getText();
 
-        Assert.assertEquals(actualResult, expectedResult);
+        Assert.assertEquals(actualResult, textString);
     }
 
 }
