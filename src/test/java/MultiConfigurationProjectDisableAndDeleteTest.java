8eb100dd8c84b5f0b1a72bd4ed37838d0bc887aa
Morigor <103515819+Morigor@users.noreply.github.com>
2022-06-14 00:21:47 +0300

    AT_046.001 | Multi-configuration project > delete created project (#776)
    * createMultiConfigurationProject() method created & testDeleteMultiConfigurationProject() added
    
    * refactoring testDeleteMultiConfigurationProject()
    
    * refactor Assert xpath at testDeleteMultiConfigurationProject()
    
    * refactoring assert in testDeleteMultiConfigurationProject()

--- src/test/java/MultiConfigurationProjectDisableAndDeleteTest.java
+++ src/test/java/MultiConfigurationProjectDisableAndDeleteTest.java
@@ -5,11 +5,11 @@ import runner.BaseTest;
 
 public class MultiConfigurationProjectDisableAndDeleteTest extends BaseTest {
 
-    @Test
-    public void testMultiConfigurationProjectMadeDisabled() {
-        String projectName = "My Multi-configuration project";
-        String projectDescription = "Creating test project";
-        String expectedResult = "This project is currently disabled";
+    private static final String PROJ_NAME = "My Multi-configuration project";
+    private static final String EXPECTED_RESULT = "This project is currently disabled";
+    private static final String PROJ_DESCRIPT = "Creating test project";
+
+    public void createMultiConfigurationProject() {
 
         getDriver()
                 .findElement(
@@ -17,24 +17,44 @@ public class MultiConfigurationProjectDisableAndDeleteTest extends BaseTest {
                 .click();
 
         getDriver().findElement(By.xpath("//div[@class='add-item-name']/input"))
-                .sendKeys(projectName);
+                .sendKeys(PROJ_NAME);
         getDriver().findElement(
                 By.xpath("//li[@class='hudson_matrix_MatrixProject']/label")).click();
         getDriver().findElement(By.id("ok-button")).click();
 
         getDriver().findElement(By.xpath("//div[@class='setting-main']/textarea[@name='description']"))
-                .sendKeys(projectDescription);
+                .sendKeys(PROJ_DESCRIPT);
         getDriver().findElement(By.xpath("//div[@class='bottom-sticker-inner']/span[@name='Submit']"))
                 .click();
+    }
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(), projectDescription);
+    @Test
+    public void testMultiConfigurationProjectMadeDisabled() {
+        createMultiConfigurationProject();
+        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='description']/div[1]")).getText(), PROJ_DESCRIPT);
 
         getDriver().findElement(By.xpath("//button[text()='Disable Project']")).click();
 
         String actualResult = getDriver()
                 .findElement(By.xpath("//div[@class='warning']")).getText();
 
-        Assert.assertTrue(actualResult.contains(expectedResult));
+        Assert.assertTrue(actualResult.contains(EXPECTED_RESULT));
+    }
+
+    @Test
+    public void testDeleteMultiConfigurationProject() {
+        createMultiConfigurationProject();
+
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
+        if(getDriver().findElement(By.id("main-panel")).getText().contains(PROJ_NAME)) {
+            getDriver().findElement(By.xpath("//a[@href='job/My%20Multi-configuration%20project/']")).click();
+            getDriver().findElement(
+                            By.xpath("//span[text()='Delete Multi-configuration project']")
+                    )
+                    .click();
+            getDriver().switchTo().alert().accept();
+        }
+
+        Assert.assertFalse(getDriver().findElement(By.id("main-panel")).getText().contains(PROJ_NAME));
     }
 }
-
