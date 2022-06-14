07c4d07d04555ea5e6c28267dc0ef7639e7807f0
Adelina <misssluv@yandex.ru>
2022-06-14 20:59:11 +0300

    AT_001.022 | New item > Create Freestyle project

---
+++ src/test/java/CreateNewFreestyleProjectTest_TC001_038.java
@@ -0,0 +1,75 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class CreateNewFreestyleProjectTest_TC001_038 extends BaseTest {
+
+    private final String NEW_JOB_FREESTYLE_CLASS = "hudson_model_FreeStyleProject";
+
+    protected void goToDashBoard() {
+        getDriver().get("http://localhost:8080/");
+    }
+
+    protected void findItemOnJobsTableAndClick(String itemName) {
+        itemName = itemName.replaceAll(" ", "%20");
+        System.out.println(itemName);
+        getDriver().findElement(By.xpath("//a[@href=\"job/".concat(itemName).concat("/\"]"))).click();
+    }
+
+    protected void deleteFreestyleProject(String itemName){
+        getDriver().get("http://localhost:8080/job/".concat(itemName).concat("/"));
+        getDriver().findElement(By.linkText("Delete Project")).click();
+        getDriver().switchTo().alert().accept();
+    }
+
+    protected void choseNewItemType(String itemType) {
+        getDriver().findElement(By.className(itemType)).click();
+    }
+
+    @Test (invocationCount = 20)
+    public void createFreestyleProjectWithValidName() {
+        String projectBName = "newFreestyleProject";
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectBName);
+        choseNewItemType(NEW_JOB_FREESTYLE_CLASS);
+        getDriver().findElement(By.id("ok-button")).click();
+        goToDashBoard();
+        findItemOnJobsTableAndClick(projectBName);
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/job/newFreestyleProject/");
+        deleteFreestyleProject(projectBName);
+    }
+
+    @Test (invocationCount = 20)
+    public void createFreestyleProjectWithInvalidName() {
+        String projectBName = "[]//*";
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectBName);
+        getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        Assert.assertTrue(getDriver().findElement(By.id("itemname-invalid")).isEnabled());
+    }
+
+    @Test (invocationCount = 20)
+    public void createFreestyleProjectWithSpacesInName() {
+        String projectBName = "new Freestyle Project";
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectBName);
+        choseNewItemType(NEW_JOB_FREESTYLE_CLASS);
+        getDriver().findElement(By.id("ok-button")).click();
+        goToDashBoard();
+        findItemOnJobsTableAndClick(projectBName);
+        Assert.assertEquals(getDriver().getCurrentUrl(), "http://localhost:8080/job/new%20Freestyle%20Project/");
+        deleteFreestyleProject(projectBName);
+    }
+    @Test (invocationCount = 20)
+    public void createFreestyleProjectNullName()  {
+        String projectBName = "lalala";
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(projectBName);
+        getDriver().findElement(By.id("name")).clear();
+        getDriver().manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        Assert.assertTrue(getDriver().findElement(By.id("itemname-required")).isEnabled());
+    }
+}
