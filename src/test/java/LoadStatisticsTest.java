3f81874d846e60383a19023c03f095ea896b4305
Julia Sabirova <58880844+jbioshock1912@users.noreply.github.com>
2022-06-14 23:21:45 +0300

    AT_118.002 | Manage Jenkins/Load statistics > Validate page elements > check buttons Short Medium Long (#818)
    Created methods: findTimeSpanList, goLoadStatisticsPage, timeSpanIsSelected;
    Added test:public void testCheckButtonsForEachTimeSpan118002.

--- src/test/java/LoadStatisticsTest.java
+++ src/test/java/LoadStatisticsTest.java
@@ -1,7 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import runner.BaseTest;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,5 +29,48 @@ public class LoadStatisticsTest extends BaseTest {
             actualToolTips.add(period.getAttribute("tooltip"));
         }
         Assert.assertEquals(actualToolTips, expectedToolTips);
+    }
+
+    @Test
+    public void testCheckButtonsForEachTimeSpan118002() {
+        SoftAssert asserts = new SoftAssert();
+
+        goLoadStatisticsPage();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(XPATH_TIME_SPAN_LIST));
+        var timeSpan = findTimeSpanList();
+
+        asserts.assertEquals(timeSpan.size(), 3);
+
+        WebElement shortButton = timeSpan.get(0);
+        WebElement mediumButton = timeSpan.get(1);
+        WebElement longButton = timeSpan.get(2);
+
+        if (timeSpanIsSelected(shortButton)) {
+            asserts.assertFalse(timeSpanIsSelected(mediumButton));
+            asserts.assertFalse(timeSpanIsSelected(longButton));
+        } else if (timeSpanIsSelected(mediumButton)) {
+            asserts.assertFalse(timeSpanIsSelected(shortButton));
+            asserts.assertFalse(timeSpanIsSelected(longButton));
+        } else {
+            asserts.assertTrue(timeSpanIsSelected(longButton));
+            asserts.assertFalse(timeSpanIsSelected(shortButton));
+            asserts.assertFalse(timeSpanIsSelected(mediumButton));
+        }
+
+        asserts.assertAll();
+    }
+
+    private List<WebElement> findTimeSpanList() {
+        return getDriver().findElements(XPATH_TIME_SPAN_LIST);
+    }
+
+    private void goLoadStatisticsPage() {
+        getDriver().findElement(XPATH_MANAGE_JENKINS).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(XPATH_LOAD_STATISTICS));
+        getDriver().findElement(XPATH_LOAD_STATISTICS).click();
+    }
+
+    private boolean timeSpanIsSelected(WebElement period) {
+        return period.getTagName().equals("span");
     }
 }
