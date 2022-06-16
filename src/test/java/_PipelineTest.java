04e4a3bb93d9a9f803a0a9335981031861db25a7
Ruslan <47644886+RuslanG13@users.noreply.github.com>
2022-06-16 21:47:31 +0300

    Rg/refactoring branch (#858)
     refactoring tests

--- src/test/java/_PipelineTest.java
+++ src/test/java/_PipelineTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,7 +16,11 @@ public class _PipelineTest extends BaseTest {
     private static final By PIPELINE = By.xpath("//span[text()='Pipeline']");
     private static final By OK_BUTTON = By.id("ok-button");
     private static final By ADVANCED_BUTTON = By.xpath("//span[@id='yui-gen4']");
+    private static final By PIPELINE_ITEM_CONFIGURATION = By.xpath(
+            "//div[@class='tab config-section-activator config_pipeline']");
     private static final String NAME_INPUT = "test123";
+    private static final String PIPELINE_PROJECT_NAME = "Ruslan Gudenko Pipeline Project+";
+
     private JavascriptExecutor javascriptExecutor;
     private Date date;
 
@@ -93,4 +98,81 @@ public class _PipelineTest extends BaseTest {
 
         Assert.assertTrue(url.contains("plugins.jenkins.io/workflow-job"));
     }
+
+    private void createPipelineProject() {
+        getDriver().findElement(By.xpath("//a[@title='New Item']")).click();
+        getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys(
+                PIPELINE_PROJECT_NAME + date.getTime());
+        getDriver().findElement(By.xpath(
+                "//li[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
+        getDriver().findElement(By.xpath("//button[@id='ok-button']")).click();
+    }
+
+    private void closeJenkinsCredProvWindowMethod() {
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView();",
+                getDriver().findElement(By.xpath("//button[@id='credentials-add-abort-button']")));
+        getDriver().navigate().back();
+        getDriver().switchTo().alert().accept();
+    }
+
+    @Test
+    public void testJenkinsCredentialsProviderWindow() {
+        createPipelineProject();
+
+        getDriver().findElement(PIPELINE_ITEM_CONFIGURATION).click();
+
+        Select pipelineScriptDropDownList = new Select(getDriver()
+                .findElement(By.xpath("//div[@class='jenkins-form-item config_pipeline active']//select")));
+        pipelineScriptDropDownList.selectByIndex(1);
+        Select scmDropDownList = new Select(getDriver()
+                .findElement(By.xpath("//div[@class='jenkins-form-item has-help']//select")));
+        scmDropDownList.selectByIndex(1);
+
+        getDriver().findElement(By.xpath("//button[@id='yui-gen15-button']")).click();
+        getDriver().findElement(By.xpath("//li[@id='yui-gen17']")).click();
+
+        WebElement titleOfJenkinsCredentialsProviderWindow = getDriver().findElement(By.xpath("//h2"));
+
+        Assert.assertEquals(titleOfJenkinsCredentialsProviderWindow.getText(), "Jenkins Credentials Provider: Jenkins");
+
+        closeJenkinsCredProvWindowMethod();
+    }
+
+    @Test
+    public void testPipelineSyntaxPageOpening() {
+        createPipelineProject();
+
+        getDriver().findElement(PIPELINE_ITEM_CONFIGURATION).click();
+
+        String pipelineSyntaxLink = getDriver().findElement(By.xpath("//a[@href='pipeline-syntax']"))
+                .getAttribute("href");
+        getDriver().get(pipelineSyntaxLink);
+
+        List<WebElement> breadcrumbsOfLinksMenu = getDriver().findElements(By.xpath("//li[@class='item']/a"));
+        String breadcrumbsPipelineSyntaxPageItem = breadcrumbsOfLinksMenu.get(2).getAttribute("href");
+
+        Assert.assertTrue(breadcrumbsPipelineSyntaxPageItem.contains("pipeline-syntax"));
+    }
+
+    @Test
+    public void testPipelineGroovyPageOpening() {
+        createPipelineProject();
+
+        getDriver().findElement(PIPELINE_ITEM_CONFIGURATION).click();
+
+        String useGroovySandboxCheckbox = getDriver().findElement(By.xpath(
+                "//input[@name='_.sandbox']")).getAttribute("checked");
+
+        Assert.assertEquals(useGroovySandboxCheckbox, "true");
+    }
+
+    @Test
+    public void testTitleConfigPageContainsProjectTitle() {
+        createPipelineProject();
+
+        String titleOfConfigurationPipelinePage = getDriver().getTitle();
+
+        Assert.assertTrue(titleOfConfigurationPipelinePage.contains(PIPELINE_PROJECT_NAME + date.getTime()));
+    }
+
 }
