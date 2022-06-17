9f75896b81c60424a123ca026ccb256136da81b4
ksutitova <103765645+ksutitova@users.noreply.github.com>
2022-06-17 09:30:33 +0300

    add test (#862)

--- src/test/java/_FreestyleTest.java
+++ src/test/java/_FreestyleTest.java
@@ -28,4 +28,31 @@ public class _FreestyleTest extends BaseTest {
         }
         Assert.assertFalse(checkProjectExists);
     }
+
+    private void createProject() {
+        getDriver().findElement(By.className("task-link-text")).click();
+        getDriver().findElement(By.xpath("//input[@name='name']")).sendKeys("FirstProject");
+
+        getDriver().findElement(By.className("label")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(
+                By.xpath("//html/body/div[5]/div/div/div/div/form/div[1]/div[12]/div/div[2]/div[2]/span[1]/span/button")
+        ).click();
+    }
+
+    private void deleteProject() {
+        getDriver().findElement(By.linkText("Delete Project")).click();
+        getDriver().switchTo().alert().accept();
+    }
+
+    @Test
+    public void testCreateFreestyleProject() {
+        String expectedResult = "FirstProject";
+        createProject();
+
+        String actualResult = getDriver().findElement(By.xpath("//ul/li/a[@href='/job/FirstProject/']")).getText();
+        Assert.assertEquals(actualResult, expectedResult);
+
+        deleteProject();
+    }
 }
