dff1cab86fc19fdd82ccb5f588f597bc487ea5f1
Nataliia Borisenko <netborgen@gmail.com>
2022-06-18 14:11:22 -0700

    RF |_MultibranchPipelineTest> CreateMultibranchPipelineWithValidDataTest 1)move my test to correct class 2)delete my incorrect class CreateMultibranchPipelineWithValidDataTest 3)delete my incorrect class CreateFolderWithInvalidDataTest

--- src/test/java/_MultibranchPipelineTest.java
+++ src/test/java/_MultibranchPipelineTest.java
@@ -105,4 +105,25 @@ public class _MultibranchPipelineTest extends BaseTest {
 
         Assert.assertTrue(scanLog.getText().contains("Finished: SUCCESS"));
     }
+
+    @Test
+    public void testCreateMultibranchPipelineWithValidData() {
+
+        getDriver().findElement(By.className("task-link-text")).click();
+
+        getDriver().findElement(By.id("name")).sendKeys("MultiPipeline");
+        getDriver().findElement(By.xpath("//span[text()='Multibranch Pipeline']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+
+        getDriver().findElement(By.xpath("//button[@id='yui-gen8-button']")).click();
+
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
+        String newName = getDriver().findElement(By.xpath("//a[@href='job/MultiPipeline/']")).getText();
+        Assert.assertEquals(newName, "MultiPipeline");
+
+        getDriver().findElement(By.xpath("//a[text()='Dashboard']")).click();
+        getDriver().findElement(By.xpath("//a[contains(@href, 'job/MultiPipeline/')]")).click();
+        getDriver().findElement(By.xpath("//span[text()='Delete Multibranch Pipeline']")).click();
+        getDriver().findElement(By.xpath("//button[text()='Yes']")).click();
+    }
 }
