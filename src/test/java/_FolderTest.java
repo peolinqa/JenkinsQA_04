d3b96baaa8b93c47725b9aff4010e87f434c8737
Rom-T <93357374+Rom-T@users.noreply.github.com>
2022-06-17 23:22:37 -0400

    RF | CreateFolderTest -> _FolderTest (#872)
    * RF | CreateFolderTest -> _FolderTest

--- src/test/java/_FolderTest.java
+++ src/test/java/_FolderTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,6 +12,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class _FolderTest extends BaseTest {
+
+    private static final By NAME = By.id("name");
 
     private final String PROP_PORT = "8080";
     private final String NAME_FOLDER = "Configure";
@@ -26,17 +29,40 @@ public class _FolderTest extends BaseTest {
     private final static String ALL_NAMES_IN_TABLE_XPATH = "//table[@id='projectstatus']/tbody/tr/td[3]/a";
     private final static String BASE_URL = "http:localhost:8080";
 
-    public void clickNewItem() {
+    private void clickNewItem() {
         getDriver().findElement(By.linkText("New Item")).click();
     }
 
-    public void clickOKButton() {
+    private void clickFolderItem() {
+        getDriver().findElement(By.xpath("//span[text()='Folder']")).click();
+    }
+
+    private void clickOKButton() {
         getDriver().findElement(By.id("ok-button")).click();
     }
 
-    private static final By NAME = By.id("name");
+    private void clickJenkinsHome() {
+        getDriver().findElement(By.id("jenkins-home-link")).click();
+    }
 
-    public static void deleteJobsWithPrefix(WebDriver driver, String prefix) {
+    private void createFolder(String nameFolder) {
+        clickNewItem();
+        getDriver().findElement(NAME).sendKeys(nameFolder);
+        clickFolderItem();
+        clickOKButton();
+        getDriver().findElement(By.id("yui-gen6-button")).click();
+        clickJenkinsHome();
+    }
+
+    private static void createFolder(WebDriver driver, String folderName) {
+        driver.findElement(By.linkText("New Item")).click();
+        driver.findElement(NAME).sendKeys(folderName);
+        driver.findElement(By.xpath("//span[text()='Folder']")).click();
+        driver.findElement(By.id("ok-button")).click();
+        driver.findElement(By.id("yui-gen6-button")).click();
+    }
+
+    private static void deleteJobsWithPrefix(WebDriver driver, String prefix) {
         driver.findElement(By.xpath(DASHBOARD_XPATH)).click();
         List<String> jobsNames = driver.findElements(By.xpath(ALL_NAMES_IN_TABLE_XPATH))
                 .stream()
@@ -61,17 +87,22 @@ public class _FolderTest extends BaseTest {
         }
     }
 
+    private void deleteFolder(String nameFolder) {
+        clickJenkinsHome();
+        getDriver().findElement(By.xpath("//a[@href='job/" + nameFolder + "/']")).click();
+        getDriver().findElement(By.xpath("//span[contains(text(),'Delete Folder')]")).click();
+        getDriver().findElement(By.id("yui-gen1-button")).click();
+    }
+
     @Test
     public void testConfigurePage() {
-        _FolderTest.deleteJobsWithPrefix(getDriver(),NAME_FOLDER);
+        _FolderTest.deleteJobsWithPrefix(getDriver(), NAME_FOLDER);
         final String expectedUrl = String.format("http://localhost:%s", PROP_PORT)
                 .concat("/job/")
                 .concat(NAME_FOLDER)
                 .concat("/");
-
         _FolderTest.createFolder(getDriver(), NAME_FOLDER);
         getWait5().until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("jenkins")));
-
         String actualURL = getDriver().getCurrentUrl();
         Assert.assertEquals(actualURL, expectedUrl);
         deleteJobsWithPrefix(getDriver(), NAME_FOLDER);
@@ -84,7 +115,6 @@ public class _FolderTest extends BaseTest {
 
         WebElement inputField = getDriver().findElement(By.id("name"));
 
-
         for (int i = 0; i < INVALID_SYMBOLS.length; i++) {
             inputField.sendKeys(Character.toString(INVALID_SYMBOLS[i]));
             WebElement warningText = getDriver().findElement(By.id("itemname-invalid"));
@@ -93,8 +123,7 @@ public class _FolderTest extends BaseTest {
                     "» ‘" + INVALID_SYMBOLS[i] + "’ is an unsafe character"));
 
             String expectedResult = "» ‘" + INVALID_SYMBOLS[i] + "’ is an unsafe character";
-            Assert.assertEquals(warningText.getText(),
-                    expectedResult);
+            Assert.assertEquals(warningText.getText(), expectedResult);
             inputField.clear();
         }
     }
@@ -135,11 +164,108 @@ public class _FolderTest extends BaseTest {
         Assert.assertEquals(folderNotFoundText, "Nothing seems to match.");
     }
 
-    public static void createFolder(WebDriver driver, String folderName) {
-        driver.findElement(By.linkText("New Item")).click();
-        driver.findElement(NAME).sendKeys(folderName);
-        driver.findElement(By.xpath("//span[text()='Folder']")).click();
-        driver.findElement(By.id("ok-button")).click();
-        driver.findElement(By.id("yui-gen6-button")).click();
+    @Test
+    public void testCreateFolderPositive() {
+
+        clickNewItem();
+
+        getDriver().findElement(NAME).sendKeys("TestRomanFolder_1");
+        clickFolderItem();
+        clickOKButton();
+
+        getDriver().findElement(By.name("_.description")).sendKeys("Description for the new folder");
+        getDriver().findElement(By.id("yui-gen6-button")).click();
+
+        getDriver().findElement(By.xpath("//a[normalize-space()='Dashboard']")).click();
+
+        boolean isFound;
+        try {
+            getDriver().findElement(By.id("job_TestRomanFolder_1"));
+            isFound = true;
+        } catch (NoSuchElementException e) {
+            isFound = false;
+        }
+
+        Assert.assertTrue(isFound);
+    }
+
+    @Test
+    public void testCreateFolderThatStartsWithUnsafeCharacter() {
+
+        final String symbols = "!@#$%^&*:;<>?/\\.";
+
+        clickNewItem();
+
+        WebElement name = getDriver().findElement(NAME);
+
+        for (int i = 0; i < symbols.length(); i++) {
+            name.clear();
+            String s = String.valueOf(symbols.charAt(i));
+            name.sendKeys(s);
+            clickFolderItem();
+
+            String expectedResult = "";
+            if (s.equals(".")) {
+                expectedResult = "» “" + s + "” is not an allowed name";
+            } else {
+                expectedResult = "» ‘" + s + "’ is an unsafe character";
+            }
+
+            String actualResult = getDriver().findElement(By.id("itemname-invalid")).getText();
+
+            Assert.assertEquals(actualResult, expectedResult);
+        }
+    }
+
+    @Test
+    public void testCreateFolderWithUnsafeCharacter() {
+
+        final String expectedErrorMessage = "» ‘@’ is an unsafe character";
+        final String expectedError = "Error";
+
+        clickNewItem();
+
+        getDriver().findElement(NAME).sendKeys("TestFolder@Jenkins");
+        clickFolderItem();
+        String actualErrorMessage1 = getDriver().findElement(By.id("itemname-invalid")).getText();
+
+        Assert.assertEquals(actualErrorMessage1, expectedErrorMessage);
+
+        clickOKButton();
+
+        String actualError = getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText();
+        String actualErrorMessage2 = getDriver().findElement(By.xpath("//div[@id='main-panel']/p")).getText();
+
+        Assert.assertEquals(actualError, expectedError);
+        Assert.assertEquals(actualErrorMessage2, expectedErrorMessage.substring(2));
+    }
+
+    @Test
+    public void testCreateFolderWithTheSameName() {
+
+        final String nameFolder = "TestRomanFolder";
+        final String expectedErrorMessage = "» A job already exists with the name ‘" + nameFolder + "’";
+        final String expectedError = "Error";
+
+        createFolder(nameFolder);
+
+        clickNewItem();
+
+        getDriver().findElement(NAME).sendKeys(nameFolder);
+        clickFolderItem();
+        WebElement actualErrorMessage1 = getWait20()
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("itemname-invalid")));
+
+        Assert.assertEquals(actualErrorMessage1.getText(), expectedErrorMessage);
+
+        clickOKButton();
+
+        String actualError = getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText();
+        String actualErrorMessage2 = getDriver().findElement(By.xpath("//div[@id='main-panel']/p")).getText();
+
+        Assert.assertEquals(actualError, expectedError);
+        Assert.assertEquals(actualErrorMessage2, expectedErrorMessage.substring(2));
+
+        deleteFolder(nameFolder);
     }
 }
