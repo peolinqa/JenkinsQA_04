fedfbf180ba9dcfc813195258971894afc103ea4
Julia Sabirova <58880844+jbioshock1912@users.noreply.github.com>
2022-06-20 13:59:49 +0300

    RF |_NewItemTest> testCheckLabelStyle, testCheckDescriptionStyle, testCheckIconAvailabilityDisplaying (#952)
    Moved tests testCheckLabelStyle, testCheckDescriptionStyle, testCheckIconAvailabilityDisplaying in new class;
    
    Delete old class SingleCategoryUITest

--- src/test/java/_NewItemTest.java
+++ src/test/java/_NewItemTest.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import runner.BaseTest;
+import java.util.List;
 
 public class _NewItemTest extends BaseTest {
 
@@ -14,6 +15,10 @@ public class _NewItemTest extends BaseTest {
     private final String NAME = "NJ";
     private final String DESCRIPTION_INPUT = "New Project created by TA";
     private final String URL_INPUT = "https://github.com/SergeiDemyanenko/JenkinsQA_04/";
+    private static final By XPATH_NEW_ITEM = By.xpath("//span[contains(text(), 'New Item')]");
+    private static final By XPATH_LIST_OF_LABEL = By.xpath("//li/label");
+    private static final By XPATH_LIST_DESC_OF_LABEL = By.xpath("//div[@class='desc']");
+    private static final By XPATH_LIST_ICON = By.xpath("//div[@class='icon']/img");
 
     public void homePage() {
         getDriver().findElement(By.id("jenkins-home-link")).click();
@@ -83,5 +88,49 @@ public class _NewItemTest extends BaseTest {
         asserts.assertEquals(getDriver().findElement(By.name(DESCRIPTION_FIELD)).getText(), DESCRIPTION_INPUT);
         asserts.assertEquals(getDriver().findElement(By.name(GITHUB_URL)).getAttribute("value"), URL_INPUT);
         asserts.assertAll();
+    }
+
+    @Test
+    public void testCheckLabelStyle() {
+        final String expectedFontWeightResult = "700";
+        final String expectedFontSizeResult = "16px";
+        final String expectedColorResult = "rgba(51, 51, 51, 1)";
+
+        getDriver().findElement(XPATH_NEW_ITEM).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(XPATH_LIST_OF_LABEL));
+        List<WebElement> labels = getDriver().findElements(XPATH_LIST_OF_LABEL);
+
+        for (WebElement value : labels) {
+            Assert.assertEquals(value.getCssValue("font-weight"), expectedFontWeightResult);
+            Assert.assertEquals(value.getCssValue("font-size"), expectedFontSizeResult);
+            Assert.assertEquals(value.getCssValue("color"), expectedColorResult);
+        }
+    }
+
+    @Test
+    public void testCheckDescriptionStyle() {
+        final String expectedFontSizeResult = "14px";
+        final String expectedColorResult = "rgba(51, 51, 51, 1)";
+
+        getDriver().findElement(XPATH_NEW_ITEM).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(XPATH_LIST_DESC_OF_LABEL));
+        List<WebElement> descriptions = getDriver().findElements(XPATH_LIST_DESC_OF_LABEL);
+
+        for (WebElement value : descriptions) {
+            Assert.assertEquals(value.getCssValue("font-size"), expectedFontSizeResult);
+            Assert.assertEquals(value.getCssValue("color"), expectedColorResult);
+        }
+    }
+
+    @Test
+    public void testCheckIconAvailabilityDisplaying() {
+        getDriver().findElement(XPATH_NEW_ITEM).click();
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(XPATH_LIST_ICON));
+        List<WebElement> icons = getDriver().findElements(XPATH_LIST_ICON);
+
+        for (WebElement icon : icons) {
+            Assert.assertTrue(icon.isDisplayed());
+            Assert.assertTrue(icon.isEnabled());
+        }
     }
 }
