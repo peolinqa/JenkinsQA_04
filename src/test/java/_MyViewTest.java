cc899a5bf3d9153207ac191b300e783a44c0ba05
olpolezhaeva <89654898+olpolezhaeva@users.noreply.github.com>
2022-06-21 21:05:00 +0300

    AT_029.002 | _MyViewTest > testDeleteViewViaTabBarFrame (#977)
    * Changed:
     -private static String NAME_OF_VIEW to VIEW_NAME.
     -private static String EDIT_NAME_OF_VIEW to EDIT_VIEW_NAME.
     -final String viewName = "Test view 1234567890123456789012345678901234567890" to private static final String VIEW_NAME;
    
     Added:
     -private static final String JOB_NAME = "Job";
     -method createJob();
     -method deleteJob();
     -testDeleteViewViaTabBarFrame;
     -testCreateNewViewCheckTabBar;
     Delete empty lines.
    
    * delete testCreateNewViewCheckTabBar;
    
    Co-authored-by: olplezhaeva <olpolezhaeva2014@gmail.com>

--- src/test/java/_MyViewTest.java
+++ src/test/java/_MyViewTest.java
@@ -1,18 +1,22 @@
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class _MyViewTest extends BaseTest {
-    private static final String NAME_OF_VIEW = "My new view";
-    private static final String EDIT_NAME_OF_VIEW = String.valueOf((int) (Math.random() * 999));
-
+    private static final String JOB_NAME = "Job";
+    private static final String VIEW_NAME = "Test view 123456789012345678901234567890123456789";
+    private static final String VIEW_NAME_1 = "My new view";
+    private static final String EDIT_VIEW_NAME = String.valueOf((int) (Math.random() * 999));
 
     private WebElement dashboardMyViews() {
         return getDriver().findElement(By.xpath("//a[contains(@href, 'me/my-views')]"));
@@ -62,35 +66,65 @@ public class _MyViewTest extends BaseTest {
         buttonSave().click();
     }
 
+    private void createJob() {
+        getDriver().findElement(By.cssSelector("[title='New Item']")).click();
+        getDriver().findElement(By.id("name")).sendKeys(JOB_NAME);
+        getDriver().findElement(By.xpath("//li[contains(@class,'FreeStyleProject')]")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.cssSelector("#breadcrumbs li a")).click();
+    }
+
+    private void deleteJob() {
+        getDriver().findElement(By.linkText(JOB_NAME)).click();
+        getDriver().findElement(By.linkText("Delete Project")).click();
+        getDriver().switchTo().alert().accept();
+    }
+
     @Test
     public void testCreateNewView() {
-        final String viewName = "Test view 1234567890123456789012345678901234567890";
         final String viewDescription = "My first Test view ;-)";
 
         WebDriver driver = getDriver();
 
         driver.findElement(By.xpath("//a[@title = 'New View']")).click();
 
-        driver.findElement(By.id("name")).sendKeys(viewName);
+        driver.findElement(By.id("name")).sendKeys(VIEW_NAME);
         driver.findElement(By.xpath("//label[text() = 'List View']")).click();
         driver.findElement(By.id("ok")).click();
 
         driver.findElement(By.name("description")).sendKeys(viewDescription);
         driver.findElement(By.id("yui-gen13-button")).click();
 
-        Assert.assertEquals(viewName, driver.findElement(By.xpath("//ul[@id='breadcrumbs']/li[@class='item']/a[starts-with(@href, '/view')]")).getText());
+        Assert.assertEquals(VIEW_NAME, driver.findElement(By.xpath("//ul[@id='breadcrumbs']/li[@class='item']/a[starts-with(@href, '/view')]")).getText());
         Assert.assertEquals(viewDescription, driver.findElement(By.xpath("//div[@id='description']/div[1]")).getText());
+    }
+
+    @Test(dependsOnMethods = {"testCreateNewView"})
+    public void testDeleteViewViaTabBarFrame() {
+
+        createJob();
+
+        getDriver().findElement(By.xpath("//div/a[contains(text(),'" + VIEW_NAME + "')]")).click();
+        getDriver().findElement(By.xpath("//a[@href='delete']")).click();
+        getDriver().findElement(By.id("yui-gen1-button")).click();
+
+        List<WebElement> listViews = getDriver().findElements(By.cssSelector("div .tab a"));
+        List<String> namesOfViews = listViews.stream().map(WebElement::getText).collect(Collectors.toList());
+
+        Assert.assertFalse(namesOfViews.contains(VIEW_NAME));
+
+        deleteJob();
     }
 
     @Test
     public void testCreateNewViewWithSelectLabelMyView() {
         getDriver().findElement(By.xpath("//a[@title='New View']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(NAME_OF_VIEW);
+        getDriver().findElement(By.id("name")).sendKeys(VIEW_NAME_1);
         getDriver().findElement(By.xpath("//label[text()='My View']")).click();
         getDriver().findElement(By.id("ok")).click();
 
         Assert.assertEquals(
-                NAME_OF_VIEW,
+                VIEW_NAME_1,
                 getDriver()
                         .findElement(By.xpath("//ul[@id='breadcrumbs']//a[contains(@href, '/view')]"))
                         .getText()
@@ -100,11 +134,11 @@ public class _MyViewTest extends BaseTest {
     @Test(dependsOnMethods = {"testCreateNewViewWithSelectLabelMyView"})
     public void testCreateNewViewWithAnExistingName() {
         getDriver().findElement(By.xpath("//a[@title='New View']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(NAME_OF_VIEW);
+        getDriver().findElement(By.id("name")).sendKeys(VIEW_NAME_1);
         getDriver().findElement(By.xpath("//label[text()='My View']")).click();
 
         String errorMessageActual = getDriver().findElement(By.className("error")).getText();
-        String errorMessageExpected = "A view already exists with the name " + '"' + NAME_OF_VIEW + '"';
+        String errorMessageExpected = "A view already exists with the name " + '"' + VIEW_NAME_1 + '"';
 
         Assert.assertEquals(errorMessageActual, errorMessageExpected);
     }
@@ -118,14 +152,14 @@ public class _MyViewTest extends BaseTest {
 
         WebElement name = getDriver().findElement(By.name("name"));
         name.clear();
-        name.sendKeys(EDIT_NAME_OF_VIEW);
+        name.sendKeys(EDIT_VIEW_NAME);
 
         getDriver().findElement(By.id("yui-gen2-button")).click();
 
         Assert.assertEquals(
-                EDIT_NAME_OF_VIEW,
+                EDIT_VIEW_NAME,
                 getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']//a[contains(@href, '"
-                                + EDIT_NAME_OF_VIEW + "')]"))
+                                + EDIT_VIEW_NAME + "')]"))
                         .getText()
         );
     }
@@ -133,7 +167,7 @@ public class _MyViewTest extends BaseTest {
     @Test(dependsOnMethods = {"testEditViewChangeName"})
     public void testDeleteCreatedView() {
         getDriver().findElement(By.xpath("//ul[@id='breadcrumbs']/li[@class='children']")).click();
-        getDriver().findElement(By.xpath("//li/a[contains(@href, '" + "/view/" + EDIT_NAME_OF_VIEW + "')]")).click();
+        getDriver().findElement(By.xpath("//li/a[contains(@href, '" + "/view/" + EDIT_VIEW_NAME + "')]")).click();
         getDriver().findElement(By.xpath("//a[@href='delete']")).click();
         getDriver().findElement(By.id("yui-gen1-button")).click();
 
@@ -144,9 +178,8 @@ public class _MyViewTest extends BaseTest {
         }
 
         Assert.assertTrue(names.get(0).isEmpty());
-        Assert.assertFalse(names.contains(EDIT_NAME_OF_VIEW));
+        Assert.assertFalse(names.contains(EDIT_VIEW_NAME));
     }
-
 
     @Test
     public void testAddDescriptionOnMyViews() {
@@ -218,8 +251,4 @@ public class _MyViewTest extends BaseTest {
         buttonHidePreview().click();
         Assert.assertFalse(textareaPreview().isDisplayed());
     }
-
-
-
-
 }
