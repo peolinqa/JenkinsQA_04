218704b4378897e549a1b3d0f8ac47a1ea270c74
Aliaksandr Dubouski <43587497+sashokdzr@users.noreply.github.com>
2022-06-21 10:26:54 +0300

    RF|134.002|CLI>Code refactoring (#973)
    Co-authored-by: Aliaksandr Dubouski <sashok2000888@gmail.com>

--- src/test/java/_JenkinsCLITest.java
+++ src/test/java/_JenkinsCLITest.java
@@ -3,7 +3,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-
 public class _JenkinsCLITest extends BaseTest {
 
     private static final String AddJobToViewElement = "//table[@class='jenkins-table sortable']/tbody/tr[1]";
@@ -21,28 +20,34 @@ public class _JenkinsCLITest extends BaseTest {
     @Test
     public void checkAddJobToViewCommandTest() {
         goToCliPage();
-        Assert.assertEquals(getDriver().findElement(By
-                        .xpath(AddJobToViewElement + "/td[2]")).getText(),
-                "Adds jobs to view.");
+
+        String getDescription = getDriver().findElement(By
+                .xpath(AddJobToViewElement + "/td[2]")).getText();
 
         getDriver().findElement(By.xpath(AddJobToViewElement + "/td[1]/a")).click();
-
         Assert.assertTrue(getDriver().findElement(By.xpath("//pre[@id='example']")).getText()
                 .contains("-webSocket add-job-to-view VIEW JOB"));
+
+        Assert.assertEquals(getDescription,
+                "Adds jobs to view.");
+
         returnToCLIPage();
     }
 
     @Test
     public void checkBuildCommandTest() {
         goToCliPage();
-        Assert.assertEquals(getDriver().findElement(By
-                        .xpath(BuildElement + "/td[2]")).getText(),
-                "Builds a job, and optionally waits until its completion.");
+
+        String getDescription = getDriver().findElement(By
+                .xpath(BuildElement + "/td[2]")).getText();
 
         getDriver().findElement(By.xpath(BuildElement + "/td[1]/a")).click();
-
         Assert.assertTrue(getDriver().findElement(By.xpath("//pre[@id='example']")).getText()
                 .contains("-webSocket build JOB [-c] [-f] [-p] [-r N] [-s] [-v] [-w]"));
+
+        Assert.assertEquals(getDescription,
+                "Builds a job, and optionally waits until its completion.");
+
         returnToCLIPage();
     }
 
