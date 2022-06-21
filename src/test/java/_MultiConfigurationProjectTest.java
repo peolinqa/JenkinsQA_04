436325622d0eb102f3289edc9452b56a06679995
Валентин Куприн (xbrookx) <72659405+ValentinKuprin@users.noreply.github.com>
2022-06-21 15:50:41 +0300

    Vk/rf moved my tests to new class multi configuration project test from deleted my create multi configuration project xbrookx test class (#975)

--- src/test/java/_MultiConfigurationProjectTest.java
+++ src/test/java/_MultiConfigurationProjectTest.java
@@ -7,11 +7,13 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class _MultiConfigurationProjectTest extends BaseTest {
 
     private final String NAME_FOLDER = "TestMultiConfigurationProject";
     private final String PROJECT_NAME = "Mcproject";
+    private final By PROJECT_ON_DAHBOARD = By.xpath("//table[@id='projectstatus']//a[normalize-space(.)='" + PROJECT_NAME + "']");
 
     private void createMultiConfigFolder(String name) {
         getDriver().findElement(By.linkText("New Item")).click();
@@ -21,7 +23,7 @@ public class _MultiConfigurationProjectTest extends BaseTest {
         getDriver().findElement(By.xpath("//button[normalize-space()='Save']")).click();
     }
 
-    private void deleteFolder(String name){
+    private void deleteFolder(String name) {
 
         Actions action = new Actions(getDriver());
         action.moveToElement(getDriver().findElement(
@@ -35,41 +37,48 @@ public class _MultiConfigurationProjectTest extends BaseTest {
     }
 
     private boolean isElementPresent(String name) {
-            boolean isPresent = false;
+        boolean isPresent = false;
 
-            List<WebElement> projectsOnDashboard = getDriver().findElements(
-                    By.xpath("//table[@id='projectstatus']//tbody//td[3]"));
-            for (WebElement jobs : projectsOnDashboard) {
-                if (jobs.getText().contains(name)) {
-                    isPresent= true;
-                }
+        List<WebElement> projectsOnDashboard = getDriver().findElements(
+                By.xpath("//table[@id='projectstatus']//tbody//td[3]"));
+        for (WebElement jobs : projectsOnDashboard) {
+            if (jobs.getText().contains(name)) {
+                isPresent = true;
             }
-            return isPresent;
         }
+        return isPresent;
+    }
 
-    private void runBuildNow(String name){
+    private void runBuildNow(String name) {
         getDriver().findElement(By.id("jenkins-home-link")).click();
-        getDriver().findElement(By.xpath("//a[contains(text(),'" +name+ "')]")).click();
+        getDriver().findElement(By.xpath("//a[contains(text(),'" + name + "')]")).click();
         getDriver().findElement(By.linkText("Build Now")).click();
     }
 
-    private void selectTopBuildInHistory(){
+    private void selectTopBuildInHistory() {
         getDriver().findElement(By.className("build-status-link")).click();
     }
 
-    private boolean isSuccesedBuildIsDipslayed(){
+    private boolean isSuccesedBuildIsDipslayed() {
         return getDriver().findElement(
                 By.xpath("//span/span/*[name()='svg' and (contains(@tooltip, 'Success'))]")).isDisplayed();
     }
 
-    private void openProjectJob(String name){
-        WebElement nameOnDashboard = getDriver().findElement(By.xpath("//a[@href='job/"+name+"/']"));
+    private void openProjectJob(String name) {
+        WebElement nameOnDashboard = getDriver().findElement(By.xpath("//a[@href='job/" + name + "/']"));
         nameOnDashboard.click();
     }
 
-    private String getFolderNameOnDashboard(String name){
+    private String getFolderNameOnDashboard(String name) {
         WebElement nameOnDashboard = getDriver().findElement(By.xpath("//tr[@id='job_" + NAME_FOLDER + "']//td[3]"));
         return nameOnDashboard.getText();
+    }
+
+    private List<String> getListProjects() {
+
+        return getDriver().findElements(
+                        By.xpath("//tbody/tr/td/a[@class='jenkins-table__link model-link inside']"))
+                .stream().map(WebElement::getText).collect(Collectors.toList());
     }
 
     @Test
@@ -81,8 +90,8 @@ public class _MultiConfigurationProjectTest extends BaseTest {
         Assert.assertEquals(getFolderNameOnDashboard(NAME_FOLDER), NAME_FOLDER);
     }
 
-    @Test(dependsOnMethods={"testCreateMultiConfigFolder"})
-    public void testBuildNow(){
+    @Test(dependsOnMethods = {"testCreateMultiConfigFolder"})
+    public void testBuildNow() {
 
         runBuildNow(NAME_FOLDER);
         getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.className("build-status-link")));
@@ -92,7 +101,7 @@ public class _MultiConfigurationProjectTest extends BaseTest {
     }
 
     @Test
-    public void testBuildNowInDisabledProject(){
+    public void testBuildNowInDisabledProject() {
         String nameTestedFolder = "disabledFolder";
         boolean isBuildNowDisplayed = false;
 
@@ -102,8 +111,8 @@ public class _MultiConfigurationProjectTest extends BaseTest {
         getDriver().findElement(By.id("yui-gen1-button")).click();
 
         List<WebElement> jobMenu = getDriver().findElements(By.xpath("//div[@id='tasks']//span[2]"));
-        for(WebElement menu : jobMenu){
-            if(menu.getText().contains("Build Now")){
+        for (WebElement menu : jobMenu) {
+            if (menu.getText().contains("Build Now")) {
                 isBuildNowDisplayed = true;
             }
         }
@@ -112,7 +121,7 @@ public class _MultiConfigurationProjectTest extends BaseTest {
     }
 
     @Test
-    public void testAddDescription(){
+    public void testAddDescription() {
         String expectedResult = "test";
         createMultiConfigFolder(PROJECT_NAME);
 
@@ -160,8 +169,8 @@ public class _MultiConfigurationProjectTest extends BaseTest {
         deleteFolder(PROJECT_NAME);
     }
 
-    @Test(dependsOnMethods={"testBuildNow"})
-    public void testDeleteMultiConfigFolder(){
+    @Test(dependsOnMethods = {"testBuildNow"})
+    public void testDeleteMultiConfigFolder() {
         String nameTestedFolder = "testToDelete";
 
         createMultiConfigFolder(nameTestedFolder);
@@ -169,5 +178,130 @@ public class _MultiConfigurationProjectTest extends BaseTest {
         deleteFolder(nameTestedFolder);
 
         Assert.assertFalse(isElementPresent(nameTestedFolder));
+    }
+
+    @Test
+    public void testCheckSubMenuConfigureAfterCreatingProject() {
+        String expectedResultDiscardOldBuilds = "Help for feature: Discard old builds";
+        String expectedResult1 = "This determines when, if ever, build records for this project should be discarded. " +
+                "Build records include the console output, archived artifacts, and any other metadata related " +
+                "to a particular build.\n" +
+                "Keeping fewer builds means less disk space will be used in the Build Record Root Directory," +
+                " which is specified on the Configure System screen.\n" +
+                "Jenkins offers two options for determining when builds should be discarded:\n" +
+                "Build age: discard builds when they reach a certain age; for example, seven days old.\n" +
+                "Build count: discard the oldest build when a certain number of builds already exist.\n" +
+                "These two options can be active at the same time, so you can keep builds for 14 days, " +
+                "but only up to a limit of 50 builds, for example. If either limit is exceeded, then any " +
+                "builds beyond that limit will be discarded.\n" +
+                "You can also ensure that important builds are kept forever, regardless of the " +
+                "setting here — click the Keep this build forever button on the build page.\n" +
+                "The last stable and last successful build are also excluded from these rules.\n" +
+                "In the Advanced section, the same options can be specified, but specifically for build " +
+                "artifacts. If enabled, build artifacts will be discarded for any builds which exceed the " +
+                "defined limits. The builds themselves will still be kept; only the associated artifacts, " +
+                "if any, will be deleted.\n" +
+                "For example, if a project builds some software and produces a large installer, which is " +
+                "archived, you may wish to always keep the console log and information about which source " +
+                "control commit was built, while for disk space reasons, you may want to keep only " +
+                "the last three installers that were built.\n" +
+                "This can make sense for projects where you can easily recreate the same artifacts later by building " +
+                "the same source control commit again.\n" +
+                "Note that Jenkins does not discard items immediately when this configuration is updated, " +
+                "or as soon as any of the configured values are exceeded; these rules are evaluated " +
+                "each time a build of this project completes.";
+        String expectedResultMessage = "Saved";
+
+        createMultiConfigFolder(PROJECT_NAME);
+        returnToMainPage();
+
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(getDriver().findElement(PROJECT_ON_DAHBOARD)).perform();
+        WebElement subMenuButton = getDriver().findElement(By.id("menuSelector"));
+        actions.moveToElement(subMenuButton).click().build().perform();
+        actions.moveToElement(getDriver().findElement(
+                By.xpath("//a[@class='yuimenuitemlabel']//span[text()='Configure']"))).click().build().perform();
+
+        WebElement helpTitle = getDriver().findElement(By.xpath("//a[contains(@tooltip, 'Discard old builds')]"));
+
+        Assert.assertEquals(helpTitle.getAttribute("title"), expectedResultDiscardOldBuilds);
+
+        helpTitle.click();
+
+        String textHelp = getDriver().findElement(By.xpath("//div[@class='help']/div")).getText();
+
+        Assert.assertEquals(textHelp, expectedResult1);
+
+        WebElement checkBoxDiscardOldBuilds = getDriver().findElement(
+                By.xpath("//label[text()='Discard old builds']/preceding-sibling::input"));
+
+        if (checkBoxDiscardOldBuilds.isSelected()) {
+        } else {
+            checkBoxDiscardOldBuilds.click();
+        }
+
+        checkBoxDiscardOldBuilds = getDriver().findElement(
+                By.xpath("//label[text()='Discard old builds']/preceding-sibling::input"));
+
+        Assert.assertTrue(checkBoxDiscardOldBuilds.isSelected());
+
+        actions.moveToElement(getDriver().findElement(By.xpath("//span[@name='Apply']"))).click().build().perform();
+
+        WebElement applyMessage = getDriver().findElement(By.xpath("//div[@id='notification-bar']/span"));
+
+        Assert.assertEquals(applyMessage.getText(), expectedResultMessage);
+
+        getWait20().until(ExpectedConditions.invisibilityOfElementLocated(By.id("notification-bar")));
+
+        returnToMainPage();
+        deleteFolder(PROJECT_NAME);
+    }
+
+    @Test
+    public void testMultiConfigurationProjectRenameUsingInvalidName() {
+        String[] invalidName =
+                new String[]{"!", "@", "#", "$", "%", "^", "&", "*", ":", ";", "\\", "/", "|", "<", ">", "?", "", " "};
+
+        createMultiConfigFolder(PROJECT_NAME);
+        returnToMainPage();
+
+        getDriver().findElement(PROJECT_ON_DAHBOARD).click();
+        getDriver().findElement(By.linkText("Rename")).click();
+
+        for (String unsafeChar : invalidName) {
+            getDriver().findElement(By.name("newName")).clear();
+            getDriver().findElement(By.name("newName")).sendKeys(unsafeChar);
+            getDriver().findElement(By.id("yui-gen1-button")).click();
+            String expectedResult = "‘" + unsafeChar + "’ is an unsafe character";
+            if ("&" == unsafeChar) {
+                expectedResult = "‘&amp;’ is an unsafe character";
+            } else if (unsafeChar == "<") {
+                expectedResult = "‘&lt;’ is an unsafe character";
+            } else if (unsafeChar == ">") {
+                expectedResult = "‘&gt;’ is an unsafe character";
+            } else if (unsafeChar == "" || unsafeChar == " ") {
+                expectedResult = "No name is specified";
+            }
+
+            String actualResult = getDriver().findElement(By.xpath("//div[@id='main-panel']/p")).getText();
+
+            Assert.assertEquals(actualResult, expectedResult);
+
+            getDriver().navigate().back();
+        }
+
+        returnToMainPage();
+        deleteFolder(PROJECT_NAME);
+    }
+
+    @Test
+    public void testDeleteMultiConfigurationProject() {
+        createMultiConfigFolder(PROJECT_NAME);
+        returnToMainPage();
+        Assert.assertTrue(getListProjects().contains(PROJECT_NAME));
+        deleteFolder(PROJECT_NAME);
+        getDriver().findElement(By.xpath("//a[@id='jenkins-home-link']")).click();
+
+        Assert.assertFalse(getListProjects().contains(PROJECT_NAME));
     }
 }
