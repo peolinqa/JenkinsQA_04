f2270f4fdcb93b4f33a16faba1b79b11998901b7
marolespb8 <103780549+marolespb8@users.noreply.github.com>
2022-06-21 14:08:54 -0400

    testOKButtonErrorDisplaying added to _NewItemTest (#981)

--- src/test/java/_NewItemTest.java
+++ src/test/java/_NewItemTest.java
@@ -133,4 +133,16 @@ public class _NewItemTest extends BaseTest {
             Assert.assertTrue(icon.isEnabled());
         }
     }
+
+    @Test
+    public void testOKButtonErrorDisplaying() {
+        final String expectedError = "» This field cannot be empty, please enter a valid name";
+
+        getDriver().findElement(By.className("task-link-text")).click();
+        getDriver().findElement(By.cssSelector("div.btn-decorator")).click();
+        String actualError = getDriver().findElement(
+                By.cssSelector("#itemname-required.input-validation-message")).getText();
+
+        Assert.assertEquals(actualError, expectedError);
+    }
 }
