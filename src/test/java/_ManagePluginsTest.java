93386bd2deb81af0ece682e02527a61a55940b78
tatauch <103478763+tatauch@users.noreply.github.com>
2022-06-26 17:30:58 -0700

    Added testManageJenkinsPluginsValidateAllTabs to the _ManagePluginsTest (#1064)
    * Added testManageJenkinsPluginsValidateAllTabs to the _ManagePluginsTest
    Refactoring _ManagePluginsTest
    
    * Added testManageJenkinsPluginsValidateAllTabs to the _ManagePluginsTest
    Refactoring _ManagePluginsTest

---
+++ src/test/java/_ManagePluginsTest.java
@@ -0,0 +1,84 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import runner.BaseTest;
+import runner.ProjectUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class _ManagePluginsTest extends BaseTest {
+    private final By NAME_FILTER = By.xpath("//thead/tr/th/a[text()='Name']");
+    private final By NAME_ARROW = By.xpath("//thead/tr/th/a[text()='Name']/span");
+    private final By PLUGIN_NAMES = By.xpath("//tbody/tr/td[2]");
+    private int getListSize() {
+        return getDriver().findElements(By.xpath("//table[@id='plugins']//tbody//tr")).size();
+    }
+
+    @Test
+    public void testManagePluginsCheckNameAndArrowUp() {
+        ProjectUtils.Dashboard.Main.ManageJenkins.click(getDriver());
+        ProjectUtils.ManageJenkins.ManagePlugins.click(getDriver());
+
+        SoftAssert asserts = new SoftAssert();
+        asserts.assertTrue(getDriver().findElement(NAME_FILTER).isDisplayed());
+        asserts.assertEquals(getDriver().findElement(NAME_ARROW).getText(), "  ↓");
+
+        getDriver().findElement(NAME_FILTER).click();
+        asserts.assertEquals(getDriver().findElement(NAME_ARROW).getText(),
+                "  ↑");
+        asserts.assertAll();
+    }
+
+    @Test
+    public void testManagePluginsCheckNameFilter(){
+        ProjectUtils.Dashboard.Main.ManageJenkins.click(getDriver());
+        ProjectUtils.ManageJenkins.ManagePlugins.click(getDriver());
+
+        Assert.assertEquals(getDriver().findElement(NAME_ARROW).getText(), "  ↓");
+
+        List<WebElement> tdTable = getDriver().findElements(PLUGIN_NAMES);
+        List<String> tdList = new ArrayList<>();
+        for (WebElement alltd : tdTable) {
+            tdList.add(alltd.getAttribute("data").toLowerCase());
+        }
+
+        List noSortList = List.copyOf(tdList);
+        Collections.sort(tdList);
+        Assert.assertEquals(tdList,noSortList);
+
+        getDriver().findElement(NAME_FILTER).click();
+        Assert.assertEquals(getDriver().findElement(NAME_ARROW).getText(), "  ↑");
+
+        List<WebElement> tdTable2 = getDriver().findElements(PLUGIN_NAMES);
+        List<String> tdList2 = new ArrayList<>();
+        for (WebElement alltd2 : tdTable2) {
+            tdList2.add(alltd2.getAttribute("data").toLowerCase());
+        }
+
+        Collections.sort(tdList, Collections.reverseOrder());
+        Assert.assertEquals(tdList, tdList2);
+    }
+
+    @Test
+    public void testManageJenkinsPluginsValidateAllTabs() {
+        ProjectUtils.Dashboard.Main.ManageJenkins.click(getDriver());
+        ProjectUtils.ManageJenkins.ManagePlugins.click(getDriver());
+
+        SoftAssert asserts = new SoftAssert();
+
+        getDriver().findElement(By.xpath("//a[contains(text(),'Updates')]")).click();
+        asserts.assertNotEquals(getListSize(), 0);
+        getDriver().findElement(By.xpath("//div[@class='tab']//a[contains(text(),'Available')]")).click();
+        asserts.assertNotEquals(getListSize(), 0);
+        getDriver().findElement(By.xpath("//div[@class='tab']//a[contains(text(),'Installed')]")).click();
+        asserts.assertNotEquals(getListSize(), 0);
+        getDriver().findElement(By.xpath("//div[@class='tab']//a[contains(text(),'Advanced')]")).click();
+        asserts.assertNotEquals(getDriver().findElement(By.className("jenkins-section__title")).getSize(),0);
+        asserts.assertAll();
+    }
+
+}
