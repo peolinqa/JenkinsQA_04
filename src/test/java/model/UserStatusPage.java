0215cbae0ed871b830f06e477e1688f88a29dafc
MariaShy <68644507+MariaShy@users.noreply.github.com>
2022-07-03 15:34:19 +0300

    RF | UserStatusPage > var and methods for userDescription (#1146)
    RF | UserStatusPage > var and methods for userDescription

--- src/test/java/model/UserStatusPage.java
+++ src/test/java/model/UserStatusPage.java
@@ -9,6 +9,18 @@ public class UserStatusPage extends BasePage {
     @FindBy(xpath = "//div[@id='main-panel']/h1")
     private WebElement userName;
 
+    @FindBy(id = "description-link")
+    private WebElement changeDescriptionButton;
+
+    @FindBy(xpath = "//div[@id='description']//textarea")
+    private WebElement descriptionTextarea;
+
+    @FindBy(id = "yui-gen1-button")
+    private WebElement descriptionSaveButton;
+
+    @FindBy(id="description")
+    private WebElement userDescription;
+
     public UserStatusPage(WebDriver driver) {
         super(driver);
     }
@@ -16,5 +28,25 @@ public class UserStatusPage extends BasePage {
     public String getUserName() {
 
         return userName.getText();
+    }
+
+    public UserStatusPage clearUserDescription(){
+        changeDescriptionButton.click();
+        descriptionTextarea.clear();
+        descriptionSaveButton.click();
+
+        return this;
+    }
+
+    public UserStatusPage addUserDescription(String newUserDescription){
+        changeDescriptionButton.click();
+        descriptionTextarea.sendKeys(newUserDescription);
+        descriptionSaveButton.click();
+
+        return this;
+    }
+
+    public String getUserDescription() {
+        return userDescription.getText();
     }
 }
