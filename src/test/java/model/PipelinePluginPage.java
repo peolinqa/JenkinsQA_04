f24407fa02f17f356ab142826c09fb2b87b7689f
Igor Klimenko <103316820+patriotby07@users.noreply.github.com>
2022-07-03 22:36:21 +0300

    RF | PipelinePluginPage> create PipelinePluginPage and add POM (#1150)

---
+++ src/test/java/model/PipelinePluginPage.java
@@ -0,0 +1,23 @@
+package model;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class PipelinePluginPage extends BasePage{
+
+    public PipelinePluginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//h1")
+    private WebElement titleRedirectedPage;
+
+    public void checkRedirectionPageAndAssert(String getText) {
+        String pageTitle = titleRedirectedPage.getText();
+        getDriver().navigate().back();
+
+        Assert.assertEquals(pageTitle, getText);
+    }
+}
