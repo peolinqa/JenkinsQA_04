51ff8d5688987f4b0474474ba768161f073672c0
olpolezhaeva <89654898+olpolezhaeva@users.noreply.github.com>
2022-08-03 22:51:37 +0300

    RF | DeleteProjectPage (#1361)
    * Created class MyViewPageSideMenuFrame.
    Moved side menu webElements and click on methods from MyViewPage to MyViewPageSideMenuFrame.
    Changed click on side menu webElements in MyViewTest, PipelineTest to method getSideMenu().
    
    * Deleted class DeleteProjectPage.
    Moved webElement yesButton and method confirmDeleteAndGoHomePage() to class BaseProjectDeleteWithConfirmPage
    
    Co-authored-by: Olpolezhaeva <olpolezhaeva2014@gmail.com>

--- src/test/java/model/base/BaseProjectDeleteWithConfirmPage.java
+++ src/test/java/model/base/BaseProjectDeleteWithConfirmPage.java
@@ -1,17 +1,28 @@
 package model.base;
 
-import model.DeleteProjectPage;
+import model.HomePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class BaseProjectDeleteWithConfirmPage<Self extends BaseProjectDeleteWithConfirmPage<?>> extends BaseProjectPage<Self> {
+
+    @FindBy(id = "yui-gen1-button")
+    protected WebElement yesButton;
 
     public BaseProjectDeleteWithConfirmPage(WebDriver driver) {
         super(driver);
     }
 
-    public DeleteProjectPage clickDeleteProject() {
+    public BaseProjectDeleteWithConfirmPage clickDeleteProject() {
         deleteButton.click();
 
-        return new DeleteProjectPage(getDriver());
+        return new BaseProjectDeleteWithConfirmPage(getDriver());
+    }
+
+    public HomePage confirmDeleteAndGoHomePage() {
+        yesButton.click();
+
+        return new HomePage(getDriver());
     }
 }
